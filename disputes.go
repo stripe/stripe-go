8f8cd6558900e47bed35d46136856251fd2103a5
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2014-08-27 18:29:45 -0700

    update to 2014-08-20

--- disputes.go
+++ disputes.go
@@ -12,7 +12,8 @@ import (
 type DisputeReason string
 
 // DisputeStatus is the list of allowed values for a discount's status.
-// Allowed values are "won", "lost", "needs_ressponse", "under_review".
+// Allowed values are "won", "lost", "needs_ressponse", "under_review",
+// "warning_needs_response", "warning_under_review", "charge_refunded".
 type DisputeStatus string
 
 const (
@@ -25,10 +26,13 @@ const (
 	Credit       DisputeReason = "credit_not_processed"
 	General      DisputeReason = "general"
 
-	Won      DisputeStatus = "won"
-	Lost     DisputeStatus = "lost"
-	Response DisputeStatus = "needs_response"
-	Review   DisputeStatus = "under_review"
+	Won             DisputeStatus = "won"
+	Lost            DisputeStatus = "lost"
+	Response        DisputeStatus = "needs_response"
+	Review          DisputeStatus = "under_review"
+	WarningResponse DisputeStatus = "warning_needs_response"
+	WarningReview   DisputeStatus = "warning_under_review"
+	ChargeRefunded  DisputeStatus = "charge_refunded"
 )
 
 // DisputeParams is the set of parameters that can be used when updating a dispute.
@@ -41,17 +45,17 @@ type DisputeParams struct {
 // Dispute is the resource representing a Stripe dispute.
 // For more details see https://stripe.com/docs/api#disputes.
 type Dispute struct {
-	Live     bool              `json:"livemode"`
-	Amount   uint64            `json:"amount"`
-	Currency Currency          `json:"currency"`
-	Charge   string            `json:"charge"`
-	Created  int64             `json:"created"`
-	Reason   DisputeReason     `json:"reason"`
-	Status   DisputeStatus     `json:"status"`
-	Tx       string            `json:"balance_transaction"`
-	Evidence string            `json:"evidence"`
-	DueDate  int64             `json:"evidence_due_by"`
-	Meta     map[string]string `json:"metadata"`
+	Live         bool              `json:"livemode"`
+	Amount       uint64            `json:"amount"`
+	Currency     Currency          `json:"currency"`
+	Charge       string            `json:"charge"`
+	Created      int64             `json:"created"`
+	Reason       DisputeReason     `json:"reason"`
+	Status       DisputeStatus     `json:"status"`
+	Transactions []*Transaction    `json:"balance_transactions"`
+	Evidence     string            `json:"evidence"`
+	DueDate      int64             `json:"evidence_due_by"`
+	Meta         map[string]string `json:"metadata"`
 }
 
 // DisputeClient is the client used to invoke dispute-related APIs.
--- stripe.go
+++ stripe.go
@@ -17,7 +17,7 @@ import (
 const uri = "https://api.stripe.com/v1"
 
 // version is the currently supported API version
-const version = "2014-08-04"
+const version = "2014-08-20"
 
 // debug is a global variable that enables additional tracing
 // to help with troubleshooting while testing.
