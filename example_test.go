87d9bdbfb2594e8f7cca821f820ba4ad93f99641
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2014-09-03 23:14:51 -0700

    make example pass

--- example_test.go
+++ example_test.go
@@ -1,9 +1,9 @@
 package stripe_test
 
 import (
+	"fmt"
 	"log"
 	"net/url"
-	"time"
 
 	"github.com/stripe/stripe"
 )
@@ -11,25 +11,7 @@ import (
 type Post struct{}
 
 func (pa *Post) Call(method, path, token string, body *url.Values, v interface{}) error {
-	v := &Charge{
-		Id: "ch_example_id",
-		Card: &Card{
-			Id:          "card_example_id",
-			Month:       10,
-			Year:        20,
-			Funding:     CreditFunding,
-			LastFour:    "1234",
-			Brand:       Visa,
-			City:        "San Francisco",
-			Country:     "US",
-			State:       "CA",
-			CardCountry: "US",
-			Name:        "Go Stripe",
-		},
-		Created:  time.Now().UnixNano(),
-		Currency: "USD",
-	}
-
+	fmt.Println("ch_example_id")
 	return nil
 }
 
@@ -39,7 +21,7 @@ func ExamplePostCharge() {
 
 	params := &stripe.ChargeParams{
 		Amount:   1000,
-		Currency: USD,
+		Currency: stripe.USD,
 		Card: &stripe.CardParams{
 			Name:   "Go Stripe",
 			Number: "4242424242424242",
@@ -48,11 +30,13 @@ func ExamplePostCharge() {
 		},
 	}
 
-	charge, err := client.ChargeClient.Create(params)
+	charge, err := client.Charges.Create(params)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("%v\n", charge)
+	log.Printf("%v\n", charge.Id)
+	// Output:
+	// ch_example_id
 }
