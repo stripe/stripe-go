1b0c90a3017b1c46814da3f732c86475d960e86f
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2014-09-03 23:28:49 -0700

    add remaining examples

--- example_test.go
+++ example_test.go
@@ -15,7 +15,7 @@ func (pa *Post) Call(method, path, token string, body *url.Values, v interface{}
 	return nil
 }
 
-func ExamplePostCharge() {
+func ExamplePost() {
 	client := &stripe.Client{}
 	client.Init("key", nil, &Post{})
 
@@ -39,4 +39,96 @@ func ExamplePostCharge() {
 	log.Printf("%v\n", charge.Id)
 	// Output:
 	// ch_example_id
+}
+
+type Get struct{}
+
+func (pa *Get) Call(method, path, token string, body *url.Values, v interface{}) error {
+	fmt.Println("ch_example_id")
+	return nil
+}
+
+func ExampleGet() {
+	client := &stripe.Client{}
+	client.Init("key", nil, &Get{})
+
+	charge, err := client.Charges.Get("ch_example_id")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%v\n", charge.Id)
+	// Output:
+	// ch_example_id
+}
+
+type Update struct{}
+
+func (pa *Update) Call(method, path, token string, body *url.Values, v interface{}) error {
+	fmt.Println("updated description")
+	return nil
+}
+
+func ExampleUpdate() {
+	client := &stripe.Client{}
+	client.Init("key", nil, &Update{})
+
+	params := &stripe.ChargeParams{
+		Desc: "updated description",
+	}
+
+	charge, err := client.Charges.Update("ch_example_id", params)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%v\n", charge.Desc)
+	// Output:
+	// updated description
+}
+
+type Delete struct{}
+
+func (pa *Delete) Call(method, path, token string, body *url.Values, v interface{}) error {
+	return nil
+}
+
+func ExampleDelete() {
+	client := &stripe.Client{}
+	client.Init("key", nil, &Delete{})
+
+	err := client.Customers.Delete("acct_example_id")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Output:
+}
+
+type List struct{}
+
+func (pa *List) Call(method, path, token string, body *url.Values, v interface{}) error {
+	fmt.Println("7")
+	return nil
+}
+
+func ExampleList() {
+	client := &stripe.Client{}
+	client.Init("key", nil, &List{})
+
+	params := &stripe.ChargeListParams{}
+	params.Filters.AddFilter("limit", "", "7")
+
+	charges, err := client.Charges.List(params)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%v\n", len(charges.Values))
+	// Output:
+	// 7
 }
