09731bea60b3c3db7d27d641c50a1c54c2c6ff77
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2014-09-03 23:07:03 -0700

    update example

--- example_test.go
+++ example_test.go
@@ -2,49 +2,52 @@ package stripe_test
 
 import (
 	"log"
+	"net/url"
 	"time"
+
+	"github.com/stripe/stripe"
 )
 
-type Post struct
+type Post struct{}
 
 func (pa *Post) Call(method, path, token string, body *url.Values, v interface{}) error {
 	v := &Charge{
 		Id: "ch_example_id",
-		Card: &Card {
-			Id: "card_example_id",
-			Month: 10,
-			Year: 20,
-			Funding: CreditFunding,
-			LastFour: "1234",
-			Brand: Visa,
-			City: "San Francisco",
-			Country: "US",
-			State: "CA",
+		Card: &Card{
+			Id:          "card_example_id",
+			Month:       10,
+			Year:        20,
+			Funding:     CreditFunding,
+			LastFour:    "1234",
+			Brand:       Visa,
+			City:        "San Francisco",
+			Country:     "US",
+			State:       "CA",
 			CardCountry: "US",
-			Name: "Go Stripe"
+			Name:        "Go Stripe",
 		},
-		Created: time.Now().UnixNano(),
-		Currency: "USD"
+		Created:  time.Now().UnixNano(),
+		Currency: "USD",
 	}
 
 	return nil
 }
 
-client := &Client{}
-client.Init("key", nil, &Post{})
-
 func ExamplePostCharge() {
-	params := &ChargeParams{
+	client := &stripe.Client{}
+	client.Init("key", nil, &Post{})
+
+	params := &stripe.ChargeParams{
 		Amount:   1000,
 		Currency: USD,
-		Card: &CardParams{
+		Card: &stripe.CardParams{
 			Name:   "Go Stripe",
 			Number: "4242424242424242",
 			Month:  "10",
 			Year:   "20",
 		},
 	}
-	
+
 	charge, err := client.ChargeClient.Create(params)
 
 	if err != nil {
@@ -53,15 +56,3 @@ func ExamplePostCharge() {
 
 	log.Printf("%v\n", charge)
 }
-
-
-
-
-
-
-
-
-
-
-
-
