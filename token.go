ce86202401fe12bc83dbf2ead41ad7245c8c3c55
Andrew Metcalf <andrew@stripe.com>
2014-09-10 17:44:07 -0700

    Add risk parameters for token creation.
    For trusted sources (e.g. Shopify) we allow them to pass certain information
    (like the original client IP address) that is relevant to risk.  We intend
    to use this functionality in the new API for Checkout.  This adds the
    parameters we need for that project.

--- token.go
+++ token.go
@@ -19,6 +19,12 @@ type TokenParams struct {
 	// Email is an undocumented parameter used by Stripe Checkout
 	// It may be removed from the API without notice.
 	Email string
+	// The following parameters are undocumented and may be removed
+	// from the API without notice.
+	PaymentUserAgent string
+	UserAgent        string
+	Referrer         string
+	Ip               string
 }
 
 // Token is the resource representing a Stripe token.
