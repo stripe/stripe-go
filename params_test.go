4b2e2454d5450b9531a28dca1ab1eba56ddf83f0
Andrew Metcalf <andrew@stripe.com>
2015-01-22 10:37:17 -0800

    Support adding arbitrary key-value params to Stripe requests.
    This adds the ability to include arbitrary key-value parameters in requests
    using the new `AddExtra` method on the `Params` type. This allows for cases
    where we're beta testing new parameters in the API but haven't documented
    them for public release.

---
+++ params_test.go
@@ -0,0 +1,43 @@
+package stripe_test
+
+import (
+	"net/url"
+	"reflect"
+	"testing"
+
+	stripe "github.com/stripe/stripe-go"
+)
+
+func TestParamsWithExtras(t *testing.T) {
+	testCases := []struct {
+		InitialBody  url.Values
+		Extras       [][2]string
+		ExpectedBody url.Values
+	}{
+		{
+			InitialBody:  url.Values{"foo": {"bar"}},
+			Extras:       [][2]string{},
+			ExpectedBody: url.Values{"foo": {"bar"}},
+		},
+		{
+			InitialBody:  url.Values{"foo": {"bar"}},
+			Extras:       [][2]string{{"foo", "baz"}, {"other", "thing"}},
+			ExpectedBody: url.Values{"foo": {"bar", "baz"}, "other": {"thing"}},
+		},
+	}
+
+	for _, testCase := range testCases {
+		p := stripe.Params{}
+
+		for _, extra := range testCase.Extras {
+			p.AddExtra(extra[0], extra[1])
+		}
+
+		body := testCase.InitialBody
+		p.AppendTo(&body)
+
+		if !reflect.DeepEqual(body, testCase.ExpectedBody) {
+			t.Fatalf("Expected body of %v but got %v.", testCase.ExpectedBody, body)
+		}
+	}
+}
