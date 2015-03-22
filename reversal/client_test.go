feae060afc7cd65b0092437bbd44b86c06d1520e
Siddarth Chandrasekaran <chandrasekaran.siddarth@gmail.com>
2015-03-22 13:26:20 -0700

    fix refs to stripe-go

--- reversal/client_test.go
+++ reversal/client_test.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"testing"
 
-	stripe "github.com/stripe/stripe-go"
-	"github.com/stripe/stripe-go/charge"
-	"github.com/stripe/stripe-go/currency"
-	"github.com/stripe/stripe-go/recipient"
-	"github.com/stripe/stripe-go/transfer"
-	. "github.com/stripe/stripe-go/utils"
+	stripe "github.com/stripe-internal/stripe-go"
+	"github.com/stripe-internal/stripe-go/charge"
+	"github.com/stripe-internal/stripe-go/currency"
+	"github.com/stripe-internal/stripe-go/recipient"
+	"github.com/stripe-internal/stripe-go/transfer"
+	. "github.com/stripe-internal/stripe-go/utils"
 )
 
 func init() {
