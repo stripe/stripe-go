a8edb11f4f0a662b34503b88b6e79b04e8da19ba
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2015-07-06 17:38:27 -0700

    bump to 6.5.0

--- stripe.go
+++ stripe.go
@@ -23,7 +23,7 @@ const (
 const apiversion = "2015-04-07"
 
 // clientversion is the binding version
-const clientversion = "6.4.2"
+const clientversion = "6.5.0"
 
 // defaultHTTPTimeout is the default timeout on the http.Client used by the library.
 // This is chosen to be consistent with the other Stripe language libraries and
