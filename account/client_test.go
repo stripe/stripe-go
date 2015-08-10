9a0275966a7d0511bc1f81937c4cf4e27929e34e
Lisardo Kist <lisardo.kist@gmail.com>
2015-08-10 11:45:28 -0300

    Fix Terms of Service update

--- account/client_test.go
+++ account/client_test.go
@@ -30,6 +30,11 @@ func TestAccountNew(t *testing.T) {
 				Year:  1990,
 			},
 		},
+		TOSAcceptance: &stripe.TOSAcceptanceParams{
+			IP:        "127.0.0.1",
+			Date:      1437578361,
+			UserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12",
+		},
 	}
 
 	_, err := New(params)
