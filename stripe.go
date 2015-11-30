9e24fce508b2af8a3afe1d00553dd6abb4cce444
Brandur <brandur@mutelight.org>
2015-11-30 12:03:35 -0800

    Bump major version to 10.0.0

--- stripe.go
+++ stripe.go
@@ -24,7 +24,7 @@ const (
 const apiversion = "2015-07-13"
 
 // clientversion is the binding version
-const clientversion = "9.0.0"
+const clientversion = "10.0.0"
 
 // defaultHTTPTimeout is the default timeout on the http.Client used by the library.
 // This is chosen to be consistent with the other Stripe language libraries and
