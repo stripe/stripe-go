0c4fd3aa73472059479bd19012e648d043815149
Brandur <brandur@mutelight.org>
2016-05-20 14:18:36 -0700

    Add a StableForm type that can support ordered duplicates
    As described in #251, using `url.Values` to contain form values is
    problematic for the Stripe API because it depends on "Rack style" form
    encoding that requires duplicated parameters to occur in a certain order
    for arrays to be correctly encoded.
    
    This patch adds a new type called `StableForm` that encodes parameters
    in the order that they were added so that arrays can be properly
    supported. It also adds a `Form` interface that's shared between
    `StableForm` and `url.Values` so that we can demonstrate this concept
    for now without updating every use of `url.Values` everywhere. For
    consistency, before the final merge we should probably move to
    `StableForm` everywhere and remove the interface (which will produce a
    messy diff).
    
    Fixes #251.

--- params.go
+++ params.go
@@ -1,6 +1,7 @@
 package stripe
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
@@ -13,6 +14,45 @@ const (
 	startafter = "starting_after"
 	endbefore  = "ending_before"
 )
+
+// Form is an abstraction of a type that can have a series of values added to
+// it and then subsequently be encoded for use in an HTTP request.
+type Form interface {
+	Add(key, value string)
+	Encode() string
+}
+
+// StableForm is a Form implementation that allows duplicate keys and encodes
+// its entries in the same order that they were added.
+type StableForm struct {
+	values []formValue
+}
+
+// Add adds a key/value tuple to the form.
+func (f *StableForm) Add(key, val string) {
+	f.values = append(f.values, formValue{key, val})
+}
+
+// Encode encodes the form's values into “URL encoded” form
+// ("bar=baz&foo=quux").
+func (f *StableForm) Encode() string {
+	var buf bytes.Buffer
+	for _, v := range f.values {
+		if buf.Len() > 0 {
+			buf.WriteByte('&')
+		}
+		buf.WriteString(url.QueryEscape(v.Key))
+		buf.WriteString("=")
+		buf.WriteString(url.QueryEscape(v.Value))
+	}
+	return buf.String()
+}
+
+// A key/value tuple for use in the StableForm type.
+type formValue struct {
+	Key   string
+	Value string
+}
 
 // Params is the structure that contains the common properties
 // of any *Params structure.
@@ -122,7 +162,7 @@ func (f *Filters) AddFilter(key, op, value string) {
 }
 
 // AppendTo adds the common parameters to the query string values.
-func (p *Params) AppendTo(body *url.Values) {
+func (p *Params) AppendTo(body Form) {
 	for k, v := range p.Meta {
 		body.Add(fmt.Sprintf("metadata[%v]", k), v)
 	}
