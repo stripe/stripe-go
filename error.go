aa0ec37dce6a1c60e352431675a6a24d66e8a0cd
Brandur <brandur@mutelight.org>
2016-05-23 09:04:30 -0700

    Overhaul the errors subsystem
    Put in error type structs for each of the possible error types that we
    can expect to come back from the API.

--- error.go
+++ error.go
@@ -3,19 +3,18 @@ package stripe
 import "encoding/json"
 
 // ErrorType is the list of allowed values for the error's type.
-// Allowed values are "invalid_request_error", "api_error", "card_error".
 type ErrorType string
 
 // ErrorCode is the list of allowed values for the error's code.
-// Allowed values are "incorrect_number", "invalid_number", "invalid_expiry_month",
-// "invalid_expiry_year", "invalid_cvc", "expired_card", "incorrect_cvc", "incorrect_zip",
-// "card_declined", "missing", "processing_error", "rate_limit".
 type ErrorCode string
 
 const (
-	InvalidRequest ErrorType = "invalid_request_error"
-	APIErr         ErrorType = "api_error"
-	CardErr        ErrorType = "card_error"
+	ErrorTypeAPI            ErrorType = "api_error"
+	ErrorTypeAPIConnection  ErrorType = "api_connection_error"
+	ErrorTypeAuthentication ErrorType = "authentication_error"
+	ErrorTypeCard           ErrorType = "card_error"
+	ErrorTypeInvalidRequest ErrorType = "invalid_request_error"
+	ErrorTypeRateLimit      ErrorType = "rate_limit_error"
 
 	IncorrectNum  ErrorCode = "incorrect_number"
 	InvalidNum    ErrorCode = "invalid_number"
@@ -31,9 +30,9 @@ const (
 	RateLimit     ErrorCode = "rate_limit"
 )
 
-// Error is the response returned when a call is unsuccessful.
+// StripeError is the response returned when a call is unsuccessful.
 // For more details see  https://stripe.com/docs/api#errors.
-type Error struct {
+type StripeError struct {
 	Type           ErrorType `json:"type"`
 	Msg            string    `json:"message"`
 	Code           ErrorCode `json:"code,omitempty"`
@@ -43,8 +42,74 @@ type Error struct {
 	ChargeID       string    `json:"charge,omitempty"`
 }
 
-// Error serializes the Error object and prints the JSON string.
-func (e *Error) Error() string {
+// Error serializes the error object to JSON and returns it as a string.
+func (e *StripeError) Error() string {
 	ret, _ := json.Marshal(e)
 	return string(ret)
+}
+
+// APIConnectionError is a failure to connect to the Stripe API.
+type APIConnectionError struct {
+	StripeError
+}
+
+// Error serializes the error object to JSON and returns it as a string.
+func (e *APIConnectionError) Error() string {
+	return e.StripeError.Error()
+}
+
+// APIError is a catch all for any errors not covered by other types (and
+// should be extremely uncommon).
+type APIError struct {
+	StripeError
+}
+
+// Error serializes the error object to JSON and returns it as a string.
+func (e *APIError) Error() string {
+	return e.StripeError.Error()
+}
+
+// AuthenticationError is a failure to properly authenticate during a request.
+type AuthenticationError struct {
+	StripeError
+}
+
+// Error serializes the error object to JSON and returns it as a string.
+func (e *AuthenticationError) Error() string {
+	return e.StripeError.Error()
+}
+
+// CardError are the most common type of error you should expect to handle.
+// They result when the user enters a card that can't be charged for some
+// reason.
+type CardError struct {
+	StripeError
+	DeclineCode string `json:"decline_code,omitempty"`
+}
+
+// Error serializes the error object to JSON and returns it as a string.
+func (e *CardError) Error() string {
+	return e.StripeError.Error()
+}
+
+// InvalidRequestError is an error that occurs when a request contains invalid
+// parameters.
+type InvalidRequestError struct {
+	StripeError
+}
+
+// Error serializes the error object to JSON and returns it as a string.
+func (e *InvalidRequestError) Error() string {
+	return e.StripeError.Error()
+}
+
+// RateLimitError occurs when the Stripe API is hit to with too many requests
+// too quickly and indicates that the current request has been rate limited.
+type RateLimitError struct {
+	StripeError
+}
+
+// Error serializes the error object to JSON and returns it as a string.
+func (e *RateLimitError) Error() string {
+	return e.StripeError.Error()
 }
--- stripe_test.go
+++ stripe_test.go
@@ -1,6 +1,8 @@
 package stripe_test
 
 import (
+	"encoding/json"
+	"net/http"
 	"testing"
 
 	stripe "github.com/stripe/stripe-go"
@@ -34,4 +36,86 @@ func TestCheckinBackendConfigurationNewRequestWithStripeAccount(t *testing.T) {
 		t.Fatalf("Expected Stripe-Account %v but got %v.",
 			TestMerchantID, req.Header.Get("Stripe-Account"))
 	}
+}
+
+func TestCheckinResponseToError(t *testing.T) {
+	c := &stripe.BackendConfiguration{URL: stripe.APIURL}
+
+	// A test response that includes a status code and request ID.
+	res := &http.Response{
+		Header: http.Header{
+			"Request-Id": []string{"request-id"},
+		},
+		StatusCode: 402,
+	}
+
+	// An error that contains expected fields which we're going to serialize to
+	// JSON and inject into our converstion function.
+	expectedErr := &stripe.StripeError{
+		Code:  stripe.Missing,
+		Msg:   "A generic error",
+		Param: "expiry_date",
+	}
+	bytes, err := json.Marshal(expectedErr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// A generic Golang error.
+	err = c.ResponseToError(res, wrapError(bytes))
+
+	// An error containing Stripe-specific fields that we cast back from the
+	// generic Golang error.
+	stripeErr := err.(*stripe.StripeError)
+
+	if expectedErr.Code != stripeErr.Code {
+		t.Fatalf("Expected code %v but got %v.", expectedErr.Code, stripeErr.Code)
+	}
+
+	if expectedErr.Msg != stripeErr.Msg {
+		t.Fatalf("Expected message %v but got %v.", expectedErr.Msg, stripeErr.Msg)
+	}
+
+	if expectedErr.Param != stripeErr.Param {
+		t.Fatalf("Expected param %v but got %v.", expectedErr.Param, stripeErr.Param)
+	}
+
+	if res.Header.Get("Request-Id") != stripeErr.RequestID {
+		t.Fatalf("Expected code %v but got %v.", res.Header.Get("Request-Id"), stripeErr.RequestID)
+	}
+
+	if res.StatusCode != stripeErr.HTTPStatusCode {
+		t.Fatalf("Expected code %v but got %v.", res.StatusCode, stripeErr.HTTPStatusCode)
+	}
+
+	// An error with a "type" field which will come back aas a specific error
+	// type.
+	expectedCardErr := &stripe.CardError{
+		StripeError: stripe.StripeError{
+			Msg:  "That card was declined",
+			Type: stripe.ErrorTypeCard,
+		},
+		DeclineCode: "decline-code",
+	}
+	bytes, err = json.Marshal(expectedCardErr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = c.ResponseToError(res, wrapError(bytes))
+	cardErr := err.(*stripe.CardError)
+
+	if expectedCardErr.DeclineCode != cardErr.DeclineCode {
+		t.Fatalf("Expected decline code %v but got %v.", expectedCardErr.DeclineCode, cardErr.DeclineCode)
+	}
+
+	if expectedCardErr.Msg != cardErr.Msg {
+		t.Fatalf("Expected message %v but got %v.", expectedCardErr.Msg, cardErr.Msg)
+	}
+}
+
+// A simple function that allows us to represent an error response from Stripe
+// which comes wrapper in a JSON object with a single field of "error".
+func wrapError(serialized []byte) []byte {
+	return []byte(`{"error":` + string(serialized) + `}`)
 }
