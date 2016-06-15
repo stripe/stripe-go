2b914af2d2af00609e2ed93016a01ac55e074d1b
Brandur <brandur@mutelight.org>
2016-06-15 11:41:04 -0700

    Bump version to 15.3.0

--- stripe.go
+++ stripe.go
@@ -23,7 +23,7 @@ const (
 const apiversion = "2016-03-07"
 
 // clientversion is the binding version
-const clientversion = "15.2.0"
+const clientversion = "15.3.0"
 
 // defaultHTTPTimeout is the default timeout on the http.Client used by the library.
 // This is chosen to be consistent with the other Stripe language libraries and
