eaeab284a942160a3fed0cad775de9585b3a5e54
Matt Drollette <matt@drollette.com>
2016-08-10 18:21:03 -0500

    support expanding destination on transfers
    consistent var names
    
    alphabetize ordering
    
    alphabits

--- transfer.go
+++ transfer.go
@@ -20,6 +20,34 @@ type TransferSourceType string
 // "account_frozen", "could_not_process", "bank_account_restricted", "invalid_currency".
 type TransferFailCode string
 
+// TransferDestinationType consts represent valid transfer destinations.
+type TransferDestinationType string
+
+const (
+	// TransferDestinationAccount is a constant representing a transfer destination
+	// which is a Stripe account.
+	TransferDestinationAccount TransferDestinationType = "account"
+
+	// TransferDestinationBankAccount is a constant representing a transfer destination
+	// which is a bank account.
+	TransferDestinationBankAccount TransferDestinationType = "bank_account"
+
+	// TransferDestinationCard is a constant representing a transfer destination
+	// which is a card.
+	TransferDestinationCard TransferDestinationType = "card"
+)
+
+// TransferDestination describes the destination of a Transfer.
+// The Type should indicate which object is fleshed out
+// For more details see https://stripe.com/docs/api/go#transfer_object
+type TransferDestination struct {
+	Type        TransferDestinationType `json:"object"`
+	ID          string                  `json:"id"`
+	Account     *Account                `json:"-"`
+	BankAccount *BankAccount            `json:"-"`
+	Card        *Card                   `json:"-"`
+}
+
 // TransferParams is the set of parameters that can be used when creating or updating a transfer.
 // For more details see https://stripe.com/docs/api#create_transfer and https://stripe.com/docs/api#update_transfer.
 type TransferParams struct {
@@ -43,28 +71,29 @@ type TransferListParams struct {
 // Transfer is the resource representing a Stripe transfer.
 // For more details see https://stripe.com/docs/api#transfers.
 type Transfer struct {
-	ID             string             `json:"id"`
-	Live           bool               `json:"livemode"`
-	Amount         int64              `json:"amount"`
-	AmountReversed int64              `json:"amount_reversed"`
-	Currency       Currency           `json:"currency"`
-	Created        int64              `json:"created"`
-	Date           int64              `json:"date"`
-	Desc           string             `json:"description"`
-	FailCode       TransferFailCode   `json:"failure_code"`
-	FailMsg        string             `json:"failure_message"`
-	Status         TransferStatus     `json:"status"`
-	Type           TransferType       `json:"type"`
-	Tx             *Transaction       `json:"balance_transaction"`
-	Meta           map[string]string  `json:"metadata"`
-	Bank           *BankAccount       `json:"bank_account"`
-	Card           *Card              `json:"card"`
-	Recipient      *Recipient         `json:"recipient"`
-	Statement      string             `json:"statement_descriptor"`
-	Reversals      *ReversalList      `json:"reversals"`
-	Reversed       bool               `json:"reversed"`
-	SourceTx       *Transaction       `json:"source_transaction"`
-	SourceType     TransferSourceType `json:"source_type"`
+	ID             string              `json:"id"`
+	Live           bool                `json:"livemode"`
+	Amount         int64               `json:"amount"`
+	AmountReversed int64               `json:"amount_reversed"`
+	Currency       Currency            `json:"currency"`
+	Created        int64               `json:"created"`
+	Date           int64               `json:"date"`
+	Desc           string              `json:"description"`
+	Dest           TransferDestination `json:"destination"`
+	FailCode       TransferFailCode    `json:"failure_code"`
+	FailMsg        string              `json:"failure_message"`
+	Status         TransferStatus      `json:"status"`
+	Type           TransferType        `json:"type"`
+	Tx             *Transaction        `json:"balance_transaction"`
+	Meta           map[string]string   `json:"metadata"`
+	Bank           *BankAccount        `json:"bank_account"`
+	Card           *Card               `json:"card"`
+	Recipient      *Recipient          `json:"recipient"`
+	Statement      string              `json:"statement_descriptor"`
+	Reversals      *ReversalList       `json:"reversals"`
+	Reversed       bool                `json:"reversed"`
+	SourceTx       *Transaction        `json:"source_transaction"`
+	SourceType     TransferSourceType  `json:"source_type"`
 }
 
 // TransferList is a list of transfers as retrieved from a list endpoint.
@@ -88,4 +117,38 @@ func (t *Transfer) UnmarshalJSON(data []byte) error {
 	}
 
 	return nil
+}
+
+// UnmarshalJSON handles deserialization of a TransferDestination.
+// This custom unmarshaling is needed because the specific
+// type of destination it refers to is specified in the JSON
+func (d *TransferDestination) UnmarshalJSON(data []byte) error {
+	type dest TransferDestination
+	var dd dest
+	err := json.Unmarshal(data, &dd)
+	if err == nil {
+		*d = TransferDestination(dd)
+
+		switch d.Type {
+		case TransferDestinationAccount:
+			json.Unmarshal(data, &d.Account)
+		case TransferDestinationBankAccount:
+			json.Unmarshal(data, &d.BankAccount)
+		case TransferDestinationCard:
+			json.Unmarshal(data, &d.Card)
+		}
+	} else {
+		// the id is surrounded by "\" characters, so strip them
+		d.ID = string(data[1 : len(data)-1])
+	}
+
+	return nil
+}
+
+// MarshalJSON handles serialization of a TransferDestination.
+// This custom marshaling is needed because we can only send a string
+// ID as a destination, even though it can be expanded to a full
+// object when retrieving
+func (d *TransferDestination) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.ID)
 }
