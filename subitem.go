24726c2c8ba60cbbea780fe60b8795f3cba299dc
Kyle Conroy <kjc@stripe.com>
2016-09-23 14:20:39 -0700

    Add subscription items to Go

---
+++ subitem.go
@@ -0,0 +1,35 @@
+package stripe
+
+// SubItemParams is the set of parameters that can be used when creating or updating a subscription item.
+// For more details see https://stripe.com/docs/api#create_subscription_item and https://stripe.com/docs/api#update_subscription_item.
+type SubItemParams struct {
+	Params
+	Sub                     string
+	Quantity                int64
+	Currency                Currency
+	Plan                    string
+	ProrationDate           int64
+	NoProrate, QuantityZero bool
+}
+
+// SubItemListParams is the set of parameters that can be used when listing invoice items.
+// For more details see https://stripe.com/docs/api#list_invoiceitems.
+type SubItemListParams struct {
+	ListParams
+	Sub string
+}
+
+// SubItem is the resource represneting a Stripe subscription item.
+// For more details see https://stripe.com/docs/api#subscription_items.
+type SubItem struct {
+	ID       string `json:"id"`
+	Plan     string `json:"plan"`
+	Quantity int64  `json:"quantitiy"`
+	Created  int64  `json:"created"`
+}
+
+// SubItemList is a list of invoice items as retrieved from a list endpoint.
+type SubItemList struct {
+	ListMeta
+	Values []*SubItem `json:"data"`
+}
