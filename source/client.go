41c507025d505bcd9a649595e9b14d3760284619
Olivier Bellone <ob@stripe.com>
2017-01-17 19:44:40 +0100

    Adds update source endpoint

--- source/client.go
+++ source/client.go
@@ -7,7 +7,7 @@ import (
 	stripe "github.com/stripe/stripe-go"
 )
 
-// Client is used to invoke /orders APIs.
+// Client is used to invoke /sources APIs.
 type Client struct {
 	B   stripe.Backend
 	Key string
@@ -93,7 +93,7 @@ func (c Client) New(params *stripe.SourceObjectParams) (*stripe.Source, error) {
 	return p, err
 }
 
-// Get returns the details of an order
+// Get returns the details of a source
 // For more details see https://stripe.com/docs/api#retrieve_source.
 func Get(id string, params *stripe.SourceObjectParams) (*stripe.Source, error) {
 	return getC().Get(id, params)
@@ -114,6 +114,62 @@ func (c Client) Get(id string, params *stripe.SourceObjectParams) (*stripe.Sourc
 	source := &stripe.Source{}
 	err := c.B.Call("GET", "/sources/"+id, c.Key, body, commonParams, source)
 	return source, err
+}
+
+// Update updates a source's properties.
+// For more details see	https://stripe.com/docs/api#update_source.
+func Update(id string, params *stripe.SourceObjectParams) (*stripe.Source, error) {
+	return getC().Update(id, params)
+}
+
+func (c Client) Update(id string, params *stripe.SourceObjectParams) (*stripe.Source, error) {
+	var body *stripe.RequestValues
+	var commonParams *stripe.Params
+
+	if params != nil {
+		body = &stripe.RequestValues{}
+		commonParams = &params.Params
+
+		// Optional fields
+		if params.Owner != nil {
+			if params.Owner.Address != nil {
+				if params.Owner.Address.Line1 != "" {
+					body.Add("owner[address][line1]", params.Owner.Address.Line1)
+				}
+				if params.Owner.Address.Line2 != "" {
+					body.Add("owner[address][line2]", params.Owner.Address.Line2)
+				}
+				if params.Owner.Address.City != "" {
+					body.Add("owner[address][city]", params.Owner.Address.City)
+				}
+				if params.Owner.Address.State != "" {
+					body.Add("owner[address][state]", params.Owner.Address.State)
+				}
+				if params.Owner.Address.PostalCode != "" {
+					body.Add("owner[address][postal_code]", params.Owner.Address.PostalCode)
+				}
+				if params.Owner.Address.Country != "" {
+					body.Add("owner[address][country]", params.Owner.Address.Country)
+				}
+			}
+			if params.Owner.Email != "" {
+				body.Add("owner[email]", params.Owner.Email)
+			}
+			if params.Owner.Name != "" {
+				body.Add("owner[name]", params.Owner.Name)
+			}
+			if params.Owner.Phone != "" {
+				body.Add("owner[phone]", params.Owner.Phone)
+			}
+		}
+
+		params.AppendTo(body)
+	}
+
+	source := &stripe.Source{}
+	err := c.B.Call("POST", "/sources/"+id, c.Key, body, commonParams, source)
+
+	return cust, err
 }
 
 func getC() Client {
