c6c9703d0590a1243e9b2d3b520efbf7ca15d53a
Brandur <brandur@mutelight.org>
2017-07-19 12:19:58 -0700

    Deconstruct test cases for better safety

--- params_test.go
+++ params_test.go
@@ -26,48 +26,59 @@ func TestCheckinRangeQueryParamsAppendTo(t *testing.T) {
 		values := &stripe.RequestValues{}
 
 		// Try it with an empty set of parameters
-		params := &stripe.RangeQueryParams{
-			GreaterThan:        10,
-			GreaterThanOrEqual: 20,
-			LesserThan:         30,
-			LesserThanOrEqual:  40,
-		}
+		params := &stripe.RangeQueryParams{GreaterThan: 99}
 		params.AppendTo(values, "created")
 
-		{
-			value := values.Get("created[gt]")
-			if len(value) != 1 || value[0] != "10" {
-				t.Fatalf(
-					"Expected encoded value of 10 for created[gt] but got %v.",
-					value)
-			}
+		value := values.Get("created[gt]")
+		if len(value) != 1 || value[0] != "99" {
+			t.Fatalf(
+				"Expected encoded value of 99 for created[gt] but got %v.",
+				value)
 		}
+	}
 
-		{
-			value := values.Get("created[gte]")
-			if len(value) != 1 || value[0] != "20" {
-				t.Fatalf(
-					"Expected encoded value of 10 for created[gte] but got %v.",
-					value)
-			}
+	{
+		values := &stripe.RequestValues{}
+
+		// Try it with an empty set of parameters
+		params := &stripe.RangeQueryParams{GreaterThanOrEqual: 99}
+		params.AppendTo(values, "created")
+
+		value := values.Get("created[gte]")
+		if len(value) != 1 || value[0] != "99" {
+			t.Fatalf(
+				"Expected encoded value of 99 for created[gte] but got %v.",
+				value)
 		}
+	}
 
-		{
-			value := values.Get("created[lt]")
-			if len(value) != 1 || value[0] != "30" {
-				t.Fatalf(
-					"Expected encoded value of 10 for created[lt] but got %v.",
-					value)
-			}
+	{
+		values := &stripe.RequestValues{}
+
+		// Try it with an empty set of parameters
+		params := &stripe.RangeQueryParams{LesserThan: 99}
+		params.AppendTo(values, "created")
+
+		value := values.Get("created[lt]")
+		if len(value) != 1 || value[0] != "99" {
+			t.Fatalf(
+				"Expected encoded value of 99 for created[lt] but got %v.",
+				value)
 		}
+	}
 
-		{
-			value := values.Get("created[lte]")
-			if len(value) != 1 || value[0] != "40" {
-				t.Fatalf(
-					"Expected encoded value of 10 for created[lte] but got %v.",
-					value)
-			}
+	{
+		values := &stripe.RequestValues{}
+
+		// Try it with an empty set of parameters
+		params := &stripe.RangeQueryParams{LesserThanOrEqual: 99}
+		params.AppendTo(values, "created")
+
+		value := values.Get("created[lte]")
+		if len(value) != 1 || value[0] != "99" {
+			t.Fatalf(
+				"Expected encoded value of 99 for created[lte] but got %v.",
+				value)
 		}
 	}
 }
