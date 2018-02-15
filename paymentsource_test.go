9e2bbdcb8a8fb182340fa166ff0de0024ac5548a
Tyler Kovacs <tyler.kovacs@gmail.com>
2018-02-15 09:35:09 -0800

    update PaymentSource.MarshalJSON to serialize bank accounts too

--- paymentsource_test.go
+++ paymentsource_test.go
@@ -1,6 +1,7 @@
 package stripe
 
 import (
+	"encoding/json"
 	"testing"
 
 	assert "github.com/stretchr/testify/require"
@@ -23,5 +24,59 @@ func TestSourceParams_AppendTo(t *testing.T) {
 		t.Logf("body = %+v", body)
 		assert.Equal(t, []string{"4242424242424242"}, body.Get("source[number]"))
 		assert.Equal(t, []string{"card"}, body.Get("source[object]"))
+	}
+}
+
+func TestPaymentSource_MarshalJSON(t *testing.T) {
+	{
+		id := "card_123"
+		name := "alice cooper"
+		paymentSource := &PaymentSource{
+			Type: PaymentSourceCard,
+			ID:   id,
+			Card: &Card{
+				ID:   id,
+				Name: name,
+			},
+		}
+
+		d, err := json.Marshal(paymentSource)
+		assert.NoError(t, err)
+		assert.NotNil(t, d)
+
+		unmarshalled := &PaymentSource{}
+		err = json.Unmarshal(d, unmarshalled)
+		assert.NoError(t, err)
+
+		assert.Equal(t, unmarshalled.ID, id)
+		assert.NotNil(t, unmarshalled.Card)
+		assert.Equal(t, unmarshalled.Card.ID, id)
+		assert.Equal(t, unmarshalled.Card.Name, name)
+	}
+
+	{
+		id := "ba_123"
+		name := "big bank"
+		paymentSource := &PaymentSource{
+			Type: PaymentSourceBankAccount,
+			ID:   id,
+			BankAccount: &BankAccount{
+				ID:   id,
+				Name: name,
+			},
+		}
+
+		d, err := json.Marshal(paymentSource)
+		assert.NoError(t, err)
+		assert.NotNil(t, d)
+
+		unmarshalled := &PaymentSource{}
+		err = json.Unmarshal(d, unmarshalled)
+		assert.NoError(t, err)
+
+		assert.Equal(t, unmarshalled.ID, id)
+		assert.NotNil(t, unmarshalled.BankAccount)
+		assert.Equal(t, unmarshalled.BankAccount.ID, id)
+		assert.Equal(t, unmarshalled.BankAccount.Name, name)
 	}
 }
