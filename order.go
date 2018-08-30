983153b7fa8868dbac0bfac6f523e1b2e22c0272
Steeve Pastorelli <spastorelli@stripe.com>
2018-08-30 15:02:01 +0100

    Handle unmarshaling of OrderItem to account for the expansion of `parent`

--- order.go
+++ order.go
@@ -30,11 +30,31 @@ type OrderItemType string
 
 // List of values that OrderItemType can take.
 const (
+	OrderItemTypeCoupon   OrderItemType = "coupon"
 	OrderItemTypeDiscount OrderItemType = "discount"
-	OrderItemTypeShipping OrderItemType = "shipping"
 	OrderItemTypeSKU      OrderItemType = "sku"
+	OrderItemTypeShipping OrderItemType = "shipping"
 	OrderItemTypeTax      OrderItemType = "tax"
 )
+
+// OrderItemParentType represents the type of order item parent
+type OrderItemParentType string
+
+// List of values that OrderItemParentType can take.
+const (
+	OrderItemParentTypeCoupon   OrderItemParentType = "coupon"
+	OrderItemParentTypeDiscount OrderItemParentType = "discount"
+	OrderItemParentTypeSKU      OrderItemParentType = "sku"
+	OrderItemParentTypeShipping OrderItemParentType = "shipping"
+	OrderItemParentTypeTax      OrderItemParentType = "tax"
+)
+
+// OrderItemParent describes the parent of an order item.
+type OrderItemParent struct {
+	ID   string              `json:"id"`
+	SKU  *SKU                `json:"-"`
+	Type OrderItemParentType `json:"object"`
+}
 
 // OrderParams is the set of parameters that can be used when creating an order.
 type OrderParams struct {
@@ -176,12 +196,12 @@ type OrderItemParams struct {
 
 // OrderItem is the resource representing an order item.
 type OrderItem struct {
-	Amount      int64         `json:"amount"`
-	Currency    Currency      `json:"currency"`
-	Description string        `json:"description"`
-	Parent      string        `json:"parent"`
-	Quantity    int64         `json:"quantity"`
-	Type        OrderItemType `json:"type"`
+	Amount      int64            `json:"amount"`
+	Currency    Currency         `json:"currency"`
+	Description string           `json:"description"`
+	Parent      *OrderItemParent `json:"parent"`
+	Quantity    int64            `json:"quantity"`
+	Type        OrderItemType    `json:"type"`
 }
 
 // SetSource adds valid sources to a OrderParams object,
@@ -190,6 +210,37 @@ func (op *OrderPayParams) SetSource(sp interface{}) error {
 	source, err := SourceParamsFor(sp)
 	op.Source = source
 	return err
+}
+
+// UnmarshalJSON handles deserialization of an OrderItemParent.
+// This custom unmarshaling is needed because the resulting
+// property may be an id or a full SKU struct if it was expanded.
+func (p *OrderItemParent) UnmarshalJSON(data []byte) error {
+	if id, ok := ParseID(data); ok {
+		p.ID = id
+		return nil
+	}
+
+	type orderItemParent OrderItemParent
+	var v orderItemParent
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+
+	var err error
+	*p = OrderItemParent(v)
+
+	switch p.Type {
+	case OrderItemParentTypeSKU:
+		// Currently only SKUs `parent` is returned as an object when expanded.
+		// For other items only IDs are returned.
+		if err = json.Unmarshal(data, &p.SKU); err != nil {
+			return err
+		}
+		p.ID = p.SKU.ID
+	}
+
+	return nil
 }
 
 // UnmarshalJSON handles deserialization of an Order.
