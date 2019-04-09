db8217fcc89c612e33b04832b687dae87d1a4c5a
Remi Jannel <remi@stripe.com>
2019-04-09 16:41:10 -0700

    Add support for TaxRate resource and APIs

---
+++ taxrate/client.go
@@ -0,0 +1,100 @@
+// Package taxrate provides the /tax_rates APIs
+package taxrate
+
+import (
+	"net/http"
+
+	stripe "github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/form"
+)
+
+// Client is used to invoke /tax_rates APIs.
+type Client struct {
+	B   stripe.Backend
+	Key string
+}
+
+// New creates a new tr.
+func New(params *stripe.TaxRateParams) (*stripe.TaxRate, error) {
+	return getC().New(params)
+}
+
+// New creates a new tr.
+func (c Client) New(params *stripe.TaxRateParams) (*stripe.TaxRate, error) {
+	tr := &stripe.TaxRate{}
+	err := c.B.Call(http.MethodPost, "/v1/tax_rates", c.Key, params, tr)
+	return tr, err
+}
+
+// Get returns the details of a tax rate.
+func Get(id string, params *stripe.TaxRateParams) (*stripe.TaxRate, error) {
+	return getC().Get(id, params)
+}
+
+// Get returns the details of a tax rate.
+func (c Client) Get(id string, params *stripe.TaxRateParams) (*stripe.TaxRate, error) {
+	path := stripe.FormatURLPath("/v1/tax_rates/%s", id)
+	tr := &stripe.TaxRate{}
+	err := c.B.Call(http.MethodGet, path, c.Key, params, tr)
+	return tr, err
+}
+
+// Update updates a tax rate's properties.
+func Update(id string, params *stripe.TaxRateParams) (*stripe.TaxRate, error) {
+	return getC().Update(id, params)
+}
+
+// Update updates a tax rate's properties.
+func (c Client) Update(id string, params *stripe.TaxRateParams) (*stripe.TaxRate, error) {
+	path := stripe.FormatURLPath("/v1/tax_rates/%s", id)
+	tr := &stripe.TaxRate{}
+	err := c.B.Call(http.MethodPost, path, c.Key, params, tr)
+	return tr, err
+}
+
+// Del removes a tax rate.
+func Del(id string, params *stripe.TaxRateParams) (*stripe.TaxRate, error) {
+	return getC().Del(id, params)
+}
+
+// Del removes a tax rate.
+func (c Client) Del(id string, params *stripe.TaxRateParams) (*stripe.TaxRate, error) {
+	path := stripe.FormatURLPath("/v1/tax_rates/%s", id)
+	tr := &stripe.TaxRate{}
+	err := c.B.Call(http.MethodDelete, path, c.Key, params, tr)
+	return tr, err
+}
+
+// List returns a list of trs.
+func List(params *stripe.TaxRateListParams) *Iter {
+	return getC().List(params)
+}
+
+// List returns a list of trs.
+func (c Client) List(listParams *stripe.TaxRateListParams) *Iter {
+	return &Iter{stripe.GetIter(listParams, func(p *stripe.Params, b *form.Values) ([]interface{}, stripe.ListMeta, error) {
+		list := &stripe.TaxRateList{}
+		err := c.B.CallRaw(http.MethodGet, "/v1/tax_rates", c.Key, b, p, list)
+
+		ret := make([]interface{}, len(list.Data))
+		for i, v := range list.Data {
+			ret[i] = v
+		}
+
+		return ret, list.ListMeta, err
+	})}
+}
+
+// Iter is an iterator for trs.
+type Iter struct {
+	*stripe.Iter
+}
+
+// TaxRate returns the tr which the iterator is currently pointing to.
+func (i *Iter) TaxRate() *stripe.TaxRate {
+	return i.Current().(*stripe.TaxRate)
+}
+
+func getC() Client {
+	return Client{stripe.GetBackend(stripe.APIBackend), stripe.Key}
+}
---
+++ taxrate/client_test.go
@@ -0,0 +1,46 @@
+package taxrate
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	stripe "github.com/stripe/stripe-go"
+	_ "github.com/stripe/stripe-go/testing"
+)
+
+func TestTaxRateGet(t *testing.T) {
+	tr, err := Get("txr_123", nil)
+	assert.Nil(t, err)
+	assert.NotNil(t, tr)
+}
+
+func TestTaxRateList(t *testing.T) {
+	i := List(&stripe.TaxRateListParams{})
+
+	// Verify that we can get at least one tr
+	assert.True(t, i.Next())
+	assert.Nil(t, i.Err())
+	assert.NotNil(t, i.TaxRate())
+}
+
+func TestTaxRateNew(t *testing.T) {
+	tr, err := New(&stripe.TaxRateParams{
+		DisplayName: stripe.String("name"),
+		Inclusive:   stripe.Bool(false),
+		Percentage:  stripe.Float64(10.15),
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, tr)
+}
+
+func TestTaxRateUpdate(t *testing.T) {
+	tr, err := Update("txr_123", &stripe.TaxRateParams{
+		Params: stripe.Params{
+			Metadata: map[string]string{
+				"foo": "bar",
+			},
+		},
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, tr)
+}
