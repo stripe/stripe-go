3a2a7ef1c6f4e1876ebe55f5be7ce042117c62c7
Olivier Bellone <ob@stripe.com>
2019-05-23 16:30:17 -0700

    Add support for radar.early_fraud_warning resource

---
+++ radar/earlyfraudwarning/client_test.go
@@ -0,0 +1,35 @@
+package earlyfraudwarning
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	stripe "github.com/stripe/stripe-go"
+	_ "github.com/stripe/stripe-go/testing"
+)
+
+func TestRadarEarlyFraudWarningGet(t *testing.T) {
+	ifr, err := Get("ifr_123", nil)
+	assert.Nil(t, err)
+	assert.NotNil(t, ifr)
+}
+
+func TestRadarEarlyFraudWarningList(t *testing.T) {
+	i := List(&stripe.RadarEarlyFraudWarningListParams{})
+
+	// Verify that we can get at least one issuer fraud record
+	assert.True(t, i.Next())
+	assert.Nil(t, i.Err())
+	assert.NotNil(t, i.RadarEarlyFraudWarning())
+}
+
+func TestRadarEarlyFraudWarningListByChargeID(t *testing.T) {
+	i := List(&stripe.RadarEarlyFraudWarningListParams{
+		Charge: stripe.String("ch_123"),
+	})
+
+	// Verify that we can get at least one issuer fraud record
+	assert.True(t, i.Next())
+	assert.Nil(t, i.Err())
+	assert.NotNil(t, i.RadarEarlyFraudWarning())
+}
