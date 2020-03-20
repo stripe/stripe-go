cc81539f53d566ba3705e5ead99e1d8e8b15ad9d
Remi Jannel <remi@stripe.com>
2020-03-19 21:58:01 -0700

    Prepare future expansion of PaymentIntent

--- charge.go
+++ charge.go
@@ -485,7 +485,7 @@ type Charge struct {
 	OnBehalfOf                *Account                    `json:"on_behalf_of"`
 	Outcome                   *ChargeOutcome              `json:"outcome"`
 	Paid                      bool                        `json:"paid"`
-	PaymentIntent             string                      `json:"payment_intent"`
+	PaymentIntent             *PaymentIntent              `json:"payment_intent"`
 	PaymentMethod             string                      `json:"payment_method"`
 	PaymentMethodDetails      *ChargePaymentMethodDetails `json:"payment_method_details"`
 	ReceiptEmail              string                      `json:"receipt_email"`
