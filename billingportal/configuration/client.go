8df126118ed94b3e5a6811ae6e9da96f698f4e68
Richard Marmorstein <richardm@stripe.com>
2021-02-19 16:16:12 -0500

    WIP: add billing portal configuration

---
+++ billingportal/configuration/client.go
@@ -0,0 +1,94 @@
+// Package configuration provides API functions related to billing portal configurations.
+package configuration
+
+import (
+	"net/http"
+
+	stripe "github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/form"
+)
+
+// Client is used to invoke /billing_portal/configurations APIs.
+type Client struct {
+	B   stripe.Backend
+	Key string
+}
+
+// New creates a new billing portal configuration.
+func New(params *stripe.BillingPortalConfigurationParams) (*stripe.BillingPortalConfiguration, error) {
+	return getC().New(params)
+}
+
+// New creates a new billing portal configuration.
+func (c Client) New(params *stripe.BillingPortalConfigurationParams) (*stripe.BillingPortalConfiguration, error) {
+	configuration := &stripe.BillingPortalConfiguration{}
+	err := c.B.Call(http.MethodPost, "/v1/billing_portal/configurations", c.Key, params, configuration)
+	return configuration, err
+}
+
+// Iter is an iterator for billing portal configurations.
+type Iter struct {
+	*stripe.Iter
+}
+
+// BillingPortalConfiguration returns the billing portal configuration which the iterator is currently pointing to.
+func (i *Iter) BillingPortalConfiguration() *stripe.BillingPortalConfiguration {
+	return i.Current().(*stripe.BillingPortalConfiguration)
+}
+
+// BillingPortalConfigurationList returns the current list object which the iterator is
+// currently using. List objects will change as new API calls are made to
+// continue pagination.
+func (i *Iter) BillingPortalConfigurationList() *stripe.BillingPortalConfigurationList {
+	return i.List().(*stripe.BillingPortalConfigurationList)
+}
+
+func getC() Client {
+	return Client{stripe.GetBackend(stripe.APIBackend), stripe.Key}
+}
+
+// Get returns the details of a billing portal configuration.
+func Get(id string, params *stripe.BillingPortalConfigurationParams) (*stripe.BillingPortalConfiguration, error) {
+	return getC().Get(id, params)
+}
+
+// Get returns the details of a billing portal configuration.
+func (c Client) Get(id string, params *stripe.BillingPortalConfigurationParams) (*stripe.BillingPortalConfiguration, error) {
+	path := stripe.FormatURLPath("/v1/billing_portal/configurations/%s", id)
+	configuration := &stripe.BillingPortalConfiguration{}
+	err := c.B.Call(http.MethodGet, path, c.Key, params, configuration)
+	return configuration, err
+}
+
+// Update updates a billing portal configuration's properties.
+func Update(id string, params *stripe.BillingPortalConfigurationParams) (*stripe.BillingPortalConfiguration, error) {
+	return getC().Update(id, params)
+}
+
+// Update updates a billing portal configuration's properties.
+func (c Client) Update(id string, params *stripe.BillingPortalConfigurationParams) (*stripe.BillingPortalConfiguration, error) {
+	path := stripe.FormatURLPath("/v1/billing_portal/configurations/%s", id)
+	configuration := &stripe.BillingPortalConfiguration{}
+	err := c.B.Call(http.MethodPost, path, c.Key, params, configuration)
+	return configuration, err
+}
+
+// List returns a list of billing portal configurations.
+func List(params *stripe.BillingPortalConfigurationListParams) *Iter {
+	return getC().List(params)
+}
+
+// List returns a list of billing portal configurations.
+func (c Client) List(listParams *stripe.BillingPortalConfigurationListParams) *Iter {
+	return &Iter{stripe.GetIter(listParams, func(p *stripe.Params, b *form.Values) ([]interface{}, stripe.ListContainer, error) {
+		list := &stripe.BillingPortalConfigurationList{}
+		err := c.B.CallRaw(http.MethodGet, "/v1/billing_portal/configurations", c.Key, b, p, list)
+
+		ret := make([]interface{}, len(list.Data))
+		for i, v := range list.Data {
+			ret[i] = v
+		}
+
+		return ret, list, err
+	})}
+}
---
+++ billingportal_configuration.go
@@ -0,0 +1,212 @@
+package stripe
+
+import "encoding/json"
+
+// BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate TODO docstring
+type BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate string
+
+const (
+	BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdateAddress  BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate = "address"
+	BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdateEmail    BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate = "email"
+	BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdatePhone    BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate = "phone"
+	BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdateShipping BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate = "shipping"
+	BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdateTaxID    BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate = "tax_id"
+)
+
+// BillingPortalConfigurationFeaturesSubscriptionCancelMode TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionCancelMode string
+
+const (
+	BillingPortalConfigurationFeaturesSubscriptionCancelModeAtPeriodEnd BillingPortalConfigurationFeaturesSubscriptionCancelMode = "at_period_end"
+	BillingPortalConfigurationFeaturesSubscriptionCancelModeImmediately BillingPortalConfigurationFeaturesSubscriptionCancelMode = "immediately"
+)
+
+// BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior string
+
+const (
+	BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehaviorAlwaysInvoice    BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior = "always_invoice"
+	BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehaviorCreateProrations BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior = "create_prorations"
+	BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehaviorNone             BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior = "none"
+)
+
+// BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate string
+
+const (
+	BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdatePrice         BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate = "price"
+	BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdatePromotionCode BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate = "promotion_code"
+	BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdateQuantity      BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate = "quantity"
+)
+
+// BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior string
+
+const (
+	BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehaviorAlwaysInvoice    BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior = "always_invoice"
+	BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehaviorCreateProrations BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior = "create_prorations"
+	BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehaviorNone             BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior = "none"
+)
+
+// BillingPortalConfigurationListParams TODO docstring
+type BillingPortalConfigurationListParams struct {
+	ListParams `form:"*"`
+	Active     *bool `form:"active"`
+	IsDefault  *bool `form:"is_default"`
+}
+
+// BillingPortalConfigurationBusinessProfileParams TODO docstring
+type BillingPortalConfigurationBusinessProfileParams struct {
+	Headline          *string `form:"headline"`
+	PrivacyPolicyURL  *string `form:"privacy_policy_url"`
+	TermsOfServiceURL *string `form:"terms_of_service_url"`
+}
+
+// BillingPortalConfigurationFeaturesCustomerUpdateParams TODO docstring
+type BillingPortalConfigurationFeaturesCustomerUpdateParams struct {
+	AllowedUpdates []*string `form:"allowed_updates"`
+	Enabled        *bool     `form:"enabled"`
+}
+
+// BillingPortalConfigurationFeaturesInvoiceHistoryParams TODO docstring
+type BillingPortalConfigurationFeaturesInvoiceHistoryParams struct {
+	Enabled *bool `form:"enabled"`
+}
+
+// BillingPortalConfigurationFeaturesPaymentMethodUpdateParams TODO docstring
+type BillingPortalConfigurationFeaturesPaymentMethodUpdateParams struct {
+	Enabled *bool `form:"enabled"`
+}
+
+// BillingPortalConfigurationFeaturesSubscriptionCancelParams TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionCancelParams struct {
+	Enabled           *bool   `form:"enabled"`
+	Mode              *string `form:"mode"`
+	ProrationBehavior *string `form:"proration_behavior"`
+}
+
+// BillingPortalConfigurationFeaturesSubscriptionUpdateProductParams TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionUpdateProductParams struct {
+	Prices  []*string `form:"prices"`
+	Product *string   `form:"product"`
+}
+
+// BillingPortalConfigurationFeaturesSubscriptionUpdateParams TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionUpdateParams struct {
+	DefaultAllowedUpdates []*string                                                            `form:"default_allowed_updates"`
+	Enabled               *bool                                                                `form:"enabled"`
+	Products              []*BillingPortalConfigurationFeaturesSubscriptionUpdateProductParams `form:"products"`
+	ProrationBehavior     *string                                                              `form:"proration_behavior"`
+}
+
+// BillingPortalConfigurationFeaturesParams TODO docstring
+type BillingPortalConfigurationFeaturesParams struct {
+	CustomerUpdate      *BillingPortalConfigurationFeaturesCustomerUpdateParams      `form:"customer_update"`
+	InvoiceHistory      *BillingPortalConfigurationFeaturesInvoiceHistoryParams      `form:"invoice_history"`
+	PaymentMethodUpdate *BillingPortalConfigurationFeaturesPaymentMethodUpdateParams `form:"payment_method_update"`
+	SubscriptionCancel  *BillingPortalConfigurationFeaturesSubscriptionCancelParams  `form:"subscription_cancel"`
+	SubscriptionUpdate  *BillingPortalConfigurationFeaturesSubscriptionUpdateParams  `form:"subscription_update"`
+}
+
+// BillingPortalConfigurationParams TODO docstring
+type BillingPortalConfigurationParams struct {
+	Params           `form:"*"`
+	Active           *bool                                            `form:"active"`
+	BusinessProfile  *BillingPortalConfigurationBusinessProfileParams `form:"business_profile"`
+	DefaultReturnURL *string                                          `form:"default_return_url"`
+	Features         *BillingPortalConfigurationFeaturesParams        `form:"features"`
+}
+
+// BillingPortalConfiguration TODO docstring
+type BillingPortalConfiguration struct {
+	APIResource
+	Active           bool                                      `json:"active"`
+	Application      string                                    `json:"application"`
+	BusinessProfile  BillingPortalConfigurationBusinessProfile `json:"business_profile"`
+	Created          int64                                     `json:"created"`
+	DefaultReturnURL string                                    `json:"default_return_url"`
+	Features         BillingPortalConfigurationFeatures        `json:"features"`
+	ID               string                                    `json:"id"`
+	IsDefault        bool                                      `json:"is_default"`
+	Livemode         bool                                      `json:"livemode"`
+	Object           string                                    `json:"object"`
+	Updated          int64                                     `json:"updated"`
+}
+
+// BillingPortalConfigurationBusinessProfile TODO docstring
+type BillingPortalConfigurationBusinessProfile struct {
+	Headline          string `json:"headline"`
+	PrivacyPolicyURL  string `json:"privacy_policy_url"`
+	TermsOfServiceURL string `json:"terms_of_service_url"`
+}
+
+// BillingPortalConfigurationFeaturesCustomerUpdate TODO docstring
+type BillingPortalConfigurationFeaturesCustomerUpdate struct {
+	AllowedUpdates []*BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate `json:"allowed_updates"`
+	Enabled        bool                                                             `json:"enabled"`
+}
+
+// BillingPortalConfigurationFeaturesInvoiceHistory TODO docstring
+type BillingPortalConfigurationFeaturesInvoiceHistory struct {
+	Enabled bool `json:"enabled"`
+}
+
+// BillingPortalConfigurationFeaturesPaymentMethodUpdate TODO docstring
+type BillingPortalConfigurationFeaturesPaymentMethodUpdate struct {
+	Enabled bool `json:"enabled"`
+}
+
+// BillingPortalConfigurationFeaturesSubscriptionCancel TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionCancel struct {
+	Enabled           bool                                                                  `json:"enabled"`
+	Mode              BillingPortalConfigurationFeaturesSubscriptionCancelMode              `json:"mode"`
+	ProrationBehavior BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior `json:"proration_behavior"`
+}
+
+// BillingPortalConfigurationFeaturesSubscriptionUpdateProduct TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionUpdateProduct struct {
+	Product string `json:"product"`
+}
+
+// BillingPortalConfigurationFeaturesSubscriptionUpdate TODO docstring
+type BillingPortalConfigurationFeaturesSubscriptionUpdate struct {
+	DefaultAllowedUpdates []*BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate `json:"default_allowed_updates"`
+	Enabled               bool                                                                        `json:"enabled"`
+	Products              []*BillingPortalConfigurationFeaturesSubscriptionUpdateProduct              `json:"products"`
+	ProrationBehavior     BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior       `json:"proration_behavior"`
+}
+
+// BillingPortalConfigurationFeatures TODO docstring
+type BillingPortalConfigurationFeatures struct {
+	CustomerUpdate      BillingPortalConfigurationFeaturesCustomerUpdate      `json:"customer_update"`
+	InvoiceHistory      BillingPortalConfigurationFeaturesInvoiceHistory      `json:"invoice_history"`
+	PaymentMethodUpdate BillingPortalConfigurationFeaturesPaymentMethodUpdate `json:"payment_method_update"`
+	SubscriptionCancel  BillingPortalConfigurationFeaturesSubscriptionCancel  `json:"subscription_cancel"`
+	SubscriptionUpdate  BillingPortalConfigurationFeaturesSubscriptionUpdate  `json:"subscription_update"`
+}
+
+// BillingPortalConfigurationList TODO docstring
+type BillingPortalConfigurationList struct {
+	APIResource
+	ListMeta
+	Data []*BillingPortalConfiguration `json:"data"`
+}
+
+// UnmarshalJSON handles deserialization of a BillingPortalConfiguration.
+// This custom unmarshaling is needed because the resulting
+// property may be an id or the full struct if it was expanded.
+func (c *BillingPortalConfiguration) UnmarshalJSON(data []byte) error {
+	if id, ok := ParseID(data); ok {
+		c.ID = id
+		return nil
+	}
+
+	type billingPortalConfiguration BillingPortalConfiguration
+	var v billingPortalConfiguration
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+
+	*c = BillingPortalConfiguration(v)
+	return nil
+}
