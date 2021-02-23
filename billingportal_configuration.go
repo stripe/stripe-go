fa1d71d6fec944e0d20ee95f5926fe34b7192272
Richard Marmorstein <richardm@stripe.com>
2021-02-22 22:53:59 -0500

    Fix docstrings and depointerize enum fields

--- billingportal_configuration.go
+++ billingportal_configuration.go
@@ -2,7 +2,8 @@ package stripe
 
 import "encoding/json"
 
-// BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate TODO docstring
+// BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate describes a
+// type of customer updates that may be supported on a portal configuration
 type BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate string
 
 // List of values that BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate can take.
@@ -14,7 +15,8 @@ const (
 	BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdateTaxID    BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate = "tax_id"
 )
 
-// BillingPortalConfigurationFeaturesSubscriptionCancelMode TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionCancelMode describes whether
+// to cancel subscriptions immediately or at the end of the billing period.
 type BillingPortalConfigurationFeaturesSubscriptionCancelMode string
 
 // List of values that BillingPortalConfigurationFeaturesSubscriptionCancelMode can take.
@@ -23,7 +25,8 @@ const (
 	BillingPortalConfigurationFeaturesSubscriptionCancelModeImmediately BillingPortalConfigurationFeaturesSubscriptionCancelMode = "immediately"
 )
 
-// BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior
+// describes whether to create prorations when canceling subscriptions.
 type BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior string
 
 // List of values that BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior can take.
@@ -33,7 +36,9 @@ const (
 	BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehaviorNone             BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior = "none"
 )
 
-// BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate
+// describes a type of subscription update that may be supported on a portal
+// configuration.
 type BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate string
 
 // List of values that BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate can take.
@@ -43,7 +48,8 @@ const (
 	BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdateQuantity      BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate = "quantity"
 )
 
-// BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior
+// determines how to handle prorations resulting from subscription updates.
 type BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior string
 
 // List of values that BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior can take.
@@ -53,50 +59,58 @@ const (
 	BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehaviorNone             BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior = "none"
 )
 
-// BillingPortalConfigurationListParams TODO docstring
+// BillingPortalConfigurationListParams is the set of parameters that can be
+// used when listing portal configurations.
 type BillingPortalConfigurationListParams struct {
 	ListParams `form:"*"`
 	Active     *bool `form:"active"`
 	IsDefault  *bool `form:"is_default"`
 }
 
-// BillingPortalConfigurationBusinessProfileParams TODO docstring
+// BillingPortalConfigurationBusinessProfileParams lets you pass the business
+// profile details associated with a portal configuration.
 type BillingPortalConfigurationBusinessProfileParams struct {
 	Headline          *string `form:"headline"`
 	PrivacyPolicyURL  *string `form:"privacy_policy_url"`
 	TermsOfServiceURL *string `form:"terms_of_service_url"`
 }
 
-// BillingPortalConfigurationFeaturesCustomerUpdateParams TODO docstring
+// BillingPortalConfigurationFeaturesCustomerUpdateParams lets you pass the
+// customer update details on a portal configuration.
 type BillingPortalConfigurationFeaturesCustomerUpdateParams struct {
 	AllowedUpdates []*string `form:"allowed_updates"`
 	Enabled        *bool     `form:"enabled"`
 }
 
-// BillingPortalConfigurationFeaturesInvoiceHistoryParams TODO docstring
+// BillingPortalConfigurationFeaturesCustomerUpdateParams lets you pass the
+// invoice history details on a portal configuration.
 type BillingPortalConfigurationFeaturesInvoiceHistoryParams struct {
 	Enabled *bool `form:"enabled"`
 }
 
-// BillingPortalConfigurationFeaturesPaymentMethodUpdateParams TODO docstring
+// BillingPortalConfigurationFeaturesPaymentMethodUpdateParams lets you pass
+// the payment method update details on a portal configuration.
 type BillingPortalConfigurationFeaturesPaymentMethodUpdateParams struct {
 	Enabled *bool `form:"enabled"`
 }
 
-// BillingPortalConfigurationFeaturesSubscriptionCancelParams TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionCancelParams lets you pass the
+// subscription cancel deetails on a portal configuration.
 type BillingPortalConfigurationFeaturesSubscriptionCancelParams struct {
 	Enabled           *bool   `form:"enabled"`
 	Mode              *string `form:"mode"`
 	ProrationBehavior *string `form:"proration_behavior"`
 }
 
-// BillingPortalConfigurationFeaturesSubscriptionUpdateProductParams TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionUpdateProductParams lets you
+// pass product details on the subscription update on a portal configuration.
 type BillingPortalConfigurationFeaturesSubscriptionUpdateProductParams struct {
 	Prices  []*string `form:"prices"`
 	Product *string   `form:"product"`
 }
 
-// BillingPortalConfigurationFeaturesSubscriptionUpdateParams TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionUpdateParams lets you pass
+// subscription update details on a portal configuration.
 type BillingPortalConfigurationFeaturesSubscriptionUpdateParams struct {
 	DefaultAllowedUpdates []*string                                                            `form:"default_allowed_updates"`
 	Enabled               *bool                                                                `form:"enabled"`
@@ -104,7 +118,8 @@ type BillingPortalConfigurationFeaturesSubscriptionUpdateParams struct {
 	ProrationBehavior     *string                                                              `form:"proration_behavior"`
 }
 
-// BillingPortalConfigurationFeaturesParams TODO docstring
+// BillingPortalConfigurationFeaturesParams lets you pass details about the
+// features available in the portal.
 type BillingPortalConfigurationFeaturesParams struct {
 	CustomerUpdate      *BillingPortalConfigurationFeaturesCustomerUpdateParams      `form:"customer_update"`
 	InvoiceHistory      *BillingPortalConfigurationFeaturesInvoiceHistoryParams      `form:"invoice_history"`
@@ -113,7 +128,8 @@ type BillingPortalConfigurationFeaturesParams struct {
 	SubscriptionUpdate  *BillingPortalConfigurationFeaturesSubscriptionUpdateParams  `form:"subscription_update"`
 }
 
-// BillingPortalConfigurationParams TODO docstring
+// BillingPortalConfigurationParams is the set of parameters that can be passed
+// when creating or updating a portal configuration.
 type BillingPortalConfigurationParams struct {
 	Params           `form:"*"`
 	Active           *bool                                            `form:"active"`
@@ -122,7 +138,9 @@ type BillingPortalConfigurationParams struct {
 	Features         *BillingPortalConfigurationFeaturesParams        `form:"features"`
 }
 
-// BillingPortalConfiguration TODO docstring
+// BillingPortalConfiguration is a configuration that describes the
+// functionality and behavior of a PortalSession. For more details see
+// https://stripe.com/docs/api/customer_portal.
 type BillingPortalConfiguration struct {
 	APIResource
 	Active           bool                                       `json:"active"`
@@ -138,51 +156,59 @@ type BillingPortalConfiguration struct {
 	Updated          int64                                      `json:"updated"`
 }
 
-// BillingPortalConfigurationBusinessProfile TODO docstring
+// BillingPortalConfigurationBusinessProfile represents the business profile
+// details on a portal configuration.
 type BillingPortalConfigurationBusinessProfile struct {
 	Headline          string `json:"headline"`
 	PrivacyPolicyURL  string `json:"privacy_policy_url"`
 	TermsOfServiceURL string `json:"terms_of_service_url"`
 }
 
-// BillingPortalConfigurationFeaturesCustomerUpdate TODO docstring
+// BillingPortalConfigurationFeaturesCustomerUpdate represents the customer
+// update details on a portal configuration.
 type BillingPortalConfigurationFeaturesCustomerUpdate struct {
 	AllowedUpdates []*BillingPortalConfigurationFeaturesCustomerUpdateAllowedUpdate `json:"allowed_updates"`
 	Enabled        bool                                                             `json:"enabled"`
 }
 
-// BillingPortalConfigurationFeaturesInvoiceHistory TODO docstring
+// BillingPortalConfigurationFeaturesInvoiceHistory represents the invoice
+// history details on a portal configuration.
 type BillingPortalConfigurationFeaturesInvoiceHistory struct {
 	Enabled bool `json:"enabled"`
 }
 
-// BillingPortalConfigurationFeaturesPaymentMethodUpdate TODO docstring
+// BillingPortalConfigurationFeaturesPaymentMethodUpdate represents the payment
+// method update details on a portal configuration.
 type BillingPortalConfigurationFeaturesPaymentMethodUpdate struct {
 	Enabled bool `json:"enabled"`
 }
 
-// BillingPortalConfigurationFeaturesSubscriptionCancel TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionCancel represents the
+// subscription cancel details on a portal configuration.
 type BillingPortalConfigurationFeaturesSubscriptionCancel struct {
-	Enabled           bool                                                                   `json:"enabled"`
-	Mode              *BillingPortalConfigurationFeaturesSubscriptionCancelMode              `json:"mode"`
-	ProrationBehavior *BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior `json:"proration_behavior"`
+	Enabled           bool                                                                  `json:"enabled"`
+	Mode              BillingPortalConfigurationFeaturesSubscriptionCancelMode              `json:"mode"`
+	ProrationBehavior BillingPortalConfigurationFeaturesSubscriptionCancelProrationBehavior `json:"proration_behavior"`
 }
 
-// BillingPortalConfigurationFeaturesSubscriptionUpdateProduct TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionUpdateProduct represents the
+// subscription update details on a portal configuration.
 type BillingPortalConfigurationFeaturesSubscriptionUpdateProduct struct {
 	Prices  []string `json:"prices"`
 	Product string   `json:"product"`
 }
 
-// BillingPortalConfigurationFeaturesSubscriptionUpdate TODO docstring
+// BillingPortalConfigurationFeaturesSubscriptionUpdate represents the
+// subscription update details on a portal configuration.
 type BillingPortalConfigurationFeaturesSubscriptionUpdate struct {
 	DefaultAllowedUpdates []*BillingPortalConfigurationFeaturesSubscriptionUpdateDefaultAllowedUpdate `json:"default_allowed_updates"`
 	Enabled               bool                                                                        `json:"enabled"`
 	Products              []*BillingPortalConfigurationFeaturesSubscriptionUpdateProduct              `json:"products"`
-	ProrationBehavior     *BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior      `json:"proration_behavior"`
+	ProrationBehavior     BillingPortalConfigurationFeaturesSubscriptionUpdateProrationBehavior       `json:"proration_behavior"`
 }
 
-// BillingPortalConfigurationFeatures TODO docstring
+// BillingPortalConfigurationFeatures represents details about the features
+// enabled in the portal configuration.
 type BillingPortalConfigurationFeatures struct {
 	CustomerUpdate      *BillingPortalConfigurationFeaturesCustomerUpdate      `json:"customer_update"`
 	InvoiceHistory      *BillingPortalConfigurationFeaturesInvoiceHistory      `json:"invoice_history"`
@@ -191,16 +217,17 @@ type BillingPortalConfigurationFeatures struct {
 	SubscriptionUpdate  *BillingPortalConfigurationFeaturesSubscriptionUpdate  `json:"subscription_update"`
 }
 
-// BillingPortalConfigurationList TODO docstring
+// BillingPortalConfigurationList is a list of portal configurations as
+// returned from a list endpoint.
 type BillingPortalConfigurationList struct {
 	APIResource
 	ListMeta
 	Data []*BillingPortalConfiguration `json:"data"`
 }
 
-// UnmarshalJSON handles deserialization of a BillingPortalConfiguration.
-// This custom unmarshaling is needed because the resulting
-// property may be an id or the full struct if it was expanded.
+// UnmarshalJSON handles deserialization of a BillingPortalConfiguration. This
+// custom unmarshaling is needed because the resulting property may be an id or
+// the full struct if it was expanded.
 func (c *BillingPortalConfiguration) UnmarshalJSON(data []byte) error {
 	if id, ok := ParseID(data); ok {
 		c.ID = id
