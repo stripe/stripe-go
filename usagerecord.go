287d8a5c3144b2105b6121dd0e1b45473665b600
Richard Marmorstein <richardm@stripe.com>
2021-03-12 16:39:51 -0500

    usagerecord.go

--- usagerecord.go
+++ usagerecord.go
@@ -1,10 +1,25 @@
+//
+//
+// File generated from our OpenAPI spec
+//
+//
+
 package stripe
 
-// Possible values for the action parameter on usage record creation.
 const (
 	UsageRecordActionIncrement string = "increment"
 	UsageRecordActionSet       string = "set"
 )
+
+// UsageRecordParams create a usage record for a specified subscription item
+// and date, and fills it with a quantity.
+type UsageRecordParams struct {
+	Params           `form:"*"`
+	SubscriptionItem *string `form:"-"` // Included in URL
+	Action           *string `form:"action"`
+	Quantity         *int64  `form:"quantity"`
+	Timestamp        *int64  `form:"timestamp"`
+}
 
 // UsageRecord represents a usage record.
 // See https://stripe.com/docs/api#usage_records
@@ -12,17 +27,8 @@ type UsageRecord struct {
 	APIResource
 	ID               string `json:"id"`
 	Livemode         bool   `json:"livemode"`
+	Object           string `json:"object"`
 	Quantity         int64  `json:"quantity"`
 	SubscriptionItem string `json:"subscription_item"`
 	Timestamp        int64  `json:"timestamp"`
-}
-
-// UsageRecordParams create a usage record for a specified subscription item
-// and date, and fills it with a quantity.
-type UsageRecordParams struct {
-	Params           `form:"*"`
-	Action           *string `form:"action"`
-	Quantity         *int64  `form:"quantity"`
-	SubscriptionItem *string `form:"-"` // passed in the URL
-	Timestamp        *int64  `form:"timestamp"`
 }
