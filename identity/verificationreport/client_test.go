c9ca6a2faaaecd07cd029b3b4a2dfa68a95a3981
Richard Marmorstein <richardm@stripe.com>
2021-05-19 01:08:04 -0400

    Tests

---
+++ identity/verificationreport/client_test.go
@@ -0,0 +1,26 @@
+package verificationreport
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	stripe "github.com/stripe/stripe-go/v72"
+	_ "github.com/stripe/stripe-go/v72/testing"
+)
+
+func TestIdentityVerificationReportGet(t *testing.T) {
+	report, err := Get("vr_123", nil)
+	assert.Nil(t, err)
+	assert.NotNil(t, report)
+	assert.Equal(t, "identity.verification_report", report.Object)
+}
+
+func TestIdentityVerificationReportList(t *testing.T) {
+	i := List(&stripe.IdentityVerificationReportListParams{})
+
+	assert.True(t, i.Next())
+	assert.Nil(t, i.Err())
+	assert.NotNil(t, i.IdentityVerificationReport())
+	assert.Equal(t, "identity.verification_report", i.IdentityVerificationReport().Object)
+	assert.NotNil(t, i.IdentityVerificationReportList())
+}
