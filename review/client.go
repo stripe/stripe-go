7a170bf94be8d9785082e7c756dbaf2cdcfcce3b
Dominic Charley-Roy <dcr@stripe.com>
2021-08-19 09:28:48 -0400

    Codegen for openapi 10ebdb9

--- review/client.go
+++ review/client.go
@@ -1,3 +1,9 @@
+//
+//
+// File generated from our OpenAPI spec
+//
+//
+
 // Package review provides the /reviews APIs
 package review
 
@@ -14,19 +20,6 @@ type Client struct {
 	Key string
 }
 
-// Approve approves a review.
-func Approve(id string, params *stripe.ReviewApproveParams) (*stripe.Review, error) {
-	return getC().Approve(id, params)
-}
-
-// Approve approves a review.
-func (c Client) Approve(id string, params *stripe.ReviewApproveParams) (*stripe.Review, error) {
-	path := stripe.FormatURLPath("/v1/reviews/%s/approve", id)
-	review := &stripe.Review{}
-	err := c.B.Call(http.MethodPost, path, c.Key, params, review)
-	return review, err
-}
-
 // Get returns the details of a review.
 func Get(id string, params *stripe.ReviewParams) (*stripe.Review, error) {
 	return getC().Get(id, params)
@@ -40,6 +33,19 @@ func (c Client) Get(id string, params *stripe.ReviewParams) (*stripe.Review, err
 	return review, err
 }
 
+// Approve is the method for the `POST /v1/reviews/{review}/approve` API.
+func Approve(id string, params *stripe.ReviewApproveParams) (*stripe.Review, error) {
+	return getC().Approve(id, params)
+}
+
+// Approve is the method for the `POST /v1/reviews/{review}/approve` API.
+func (c Client) Approve(id string, params *stripe.ReviewApproveParams) (*stripe.Review, error) {
+	path := stripe.FormatURLPath("/v1/reviews/%s/approve", id)
+	review := &stripe.Review{}
+	err := c.B.Call(http.MethodPost, path, c.Key, params, review)
+	return review, err
+}
+
 // List returns a list of reviews.
 func List(params *stripe.ReviewListParams) *Iter {
 	return getC().List(params)
@@ -47,17 +53,19 @@ func List(params *stripe.ReviewListParams) *Iter {
 
 // List returns a list of reviews.
 func (c Client) List(listParams *stripe.ReviewListParams) *Iter {
-	return &Iter{stripe.GetIter(listParams, func(p *stripe.Params, b *form.Values) ([]interface{}, stripe.ListContainer, error) {
-		list := &stripe.ReviewList{}
-		err := c.B.CallRaw(http.MethodGet, "/v1/reviews", c.Key, b, p, list)
+	return &Iter{
+		Iter: stripe.GetIter(listParams, func(p *stripe.Params, b *form.Values) ([]interface{}, stripe.ListContainer, error) {
+			list := &stripe.ReviewList{}
+			err := c.B.CallRaw(http.MethodGet, "/v1/reviews", c.Key, b, p, list)
 
-		ret := make([]interface{}, len(list.Data))
-		for i, v := range list.Data {
-			ret[i] = v
-		}
+			ret := make([]interface{}, len(list.Data))
+			for i, v := range list.Data {
+				ret[i] = v
+			}
 
-		return ret, list, err
-	})}
+			return ret, list, err
+		}),
+	}
 }
 
 // Iter is an iterator for reviews.
@@ -70,9 +78,9 @@ func (i *Iter) Review() *stripe.Review {
 	return i.Current().(*stripe.Review)
 }
 
-// ReviewList returns the current list object which the iterator is currently
-// using. List objects will change as new API calls are made to continue
-// pagination.
+// ReviewList returns the current list object which the iterator is
+// currently using. List objects will change as new API calls are made to
+// continue pagination.
 func (i *Iter) ReviewList() *stripe.ReviewList {
 	return i.List().(*stripe.ReviewList)
 }
