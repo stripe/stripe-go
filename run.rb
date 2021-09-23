3f887c12d6857f5a3c415868b1df61482050f8fd
Michael Bianco <mbianco@stripe.com>
2021-09-22 18:38:28 -0600

    Recurring orders are working

--- run.rb
+++ run.rb
@@ -86,20 +86,106 @@ def sf_metadata(sf_object)
 end
 
 def example_sf_order
-  sf.find('Order', '8015e000000IJ1rAAG')
+  # sf.find('Order', '8015e000000IJ1rAAG')
+
+  # order with recurring item
+  # sf.find('Order', '8015e000000IJDxAAO')
+
+  sf.find('Order', '8015e000000IJF5AAO')
 end
 
+CPQ_QUOTE = 'SBQQ__Quote__c'.freeze
+CPQ_QUOTE_OPPORTUNITY = 'SBQQ__Opportunity2__c'.freeze
+CPQ_QUOTE_ORDERED = 'SBQQ__Ordered__c'.freeze
+CPQ_QUOTE_PRIMARY = 'SBQQ__Primary__c'.freeze
+CPQ_QUOTE_LINE = 'SBQQ__QuoteLine__c'.freeze
+CPQ_QUOTE_LINE_PRODUCT = 'SBQQ__Product__c'.freeze
+CPQ_QUOTE_LINE_PRICEBOOK_ENTRY = 'SBQQ__PricebookEntryId__c'.freeze
+
 def create_salesforce_order
-  account_id = salesforce_client.create!('Account', Name: "REST Customer #{DateTime.now}")
-  contract_id = salesforce_client.create!('Contract', accountId: account_id)
-  order_id = salesforce_client.create!('Order', {Status: "Draft", EffectiveDate: "2021-09-21", AccountId: account_id, ContractId: contract_id})
+  product_id = '01t5e000003DsarAAC'
+  pricebook_entry_id = '01u5e000000jGn6AAE'
+
+  account_id = sf.create!('Account', Name: "REST Customer #{DateTime.now}")
+  opportunity_id = sf.create!('Opportunity', {Name: "REST Oppt #{DateTime.now}", "CloseDate": DateTime.now.iso8601, AccountId: account_id, StageName: "Closed/Won"})
+
+  # you can create a quote without *any* fields, which seems completely silly
+  quote_id = sf.create!(CPQ_QUOTE, {
+    "SBQQ__Opportunity2__c": opportunity_id,
+    CPQ_QUOTE_PRIMARY => true
+  })
+
+  # https://developer.salesforce.com/docs/atlas.en-us.cpq_api_dev.meta/cpq_api_dev/cpq_api_read_quote.htm
+  # get CPQ version of the quote
+  cpq_quote_representation = JSON.parse(sf.get("services/apexrest/SBQQ/ServiceRouter?reader=SBQQ.QuoteAPI.QuoteReader&uid=#{quote_id}").body)
+
+  cpq_product_representation = JSON.parse(sf.patch("services/apexrest/SBQQ/ServiceRouter?loader=SBQQ.ProductAPI.ProductLoader&uid=#{product_id}", {
+    context: {
+      # productId: product_id,
+      pricebookId: pricebook_entry_id,
+      # currencyCode:
+    }.to_json
+  }).body)
+
+  # https://gist.github.com/paustint/bd18bd281134a180e014829b49ed043a
+  updated_quote = JSON.parse(sf.patch('/services/apexrest/SBQQ/ServiceRouter?loader=SBQQ.QuoteAPI.QuoteProductAdder', {
+    context: {
+        "quote": cpq_quote_representation,
+        "products": [
+          cpq_product_representation
+        ],
+        # "groupKey": 0,
+        "ignoreCalculate": true
+      # quote: {
+      #   record: {
+      #     Id: quote_id,
+      #     attributes: {
+      #       type: CPQ_QUOTE,
+      #     }
+      #   }
+      # },
+      # products: [
+      #   {
+      #     record: {
+      #       Id: product_id
+      #     }
+      #   }
+      # ],
+      # ignoreCalculate: true
+    }.to_json,
+  }).body)
+
+  # https://developer.salesforce.com/docs/atlas.en-us.cpq_dev_api.meta/cpq_dev_api/cpq_quote_api_save_final.htm
+  sf.post('/services/apexrest/SBQQ/ServiceRouter', {
+    "saver": "SBQQ.QuoteAPI.QuoteSaver",
+    "model": updated_quote.to_json
+  })
+
+  # sf.create!(CPQ_QUOTE_LINE, {
+  #   CPQ_QUOTE => quote_id,
+  #   CPQ_QUOTE_LINE_PRODUCT => product_id,
+  #   CPQ_QUOTE_LINE_PRICEBOOK_ENTRY => pricebook_entry_id
+  # })
+
+  # give CPQ some time to calculate...
+  sleep(5)
+
+  # it looks like there is additional field validation triggered here when `ordered` is set to true
+  sf.update!(CPQ_QUOTE, 'Id' => quote_id, CPQ_QUOTE_ORDERED => true)
+
+
+  binding.pry
+  # contract_id = salesforce_client.create!('Contract', accountId: account_id)
+  # order_id = salesforce_client.create!('Order', {Status: "Draft", EffectiveDate: "2021-09-21", AccountId: account_id, ContractId: contract_id})
 end
 
 def create_customer_from_sf_customer(sf_customer)
   # TODO pull contact information?
 
   customer = Stripe::Customer.create({
-    # email: email,
+    # TODO to be pulled from the contact
+    email: "salesforce+#{Time.now.to_i}@stripe.com",
+
     name: sf_customer.Name,
     metadata: {
       salesforce_id: sf_customer.Id,
@@ -138,6 +224,17 @@ def create_price_for_order_item(sf_order_item)
     raise 'unit prices should not be different'
   end
 
+  # TODO check for float quantities
+
+  optional_params = {}
+
+  if recurring_item?(sf_order_item)
+    optional_params[:recurring] = {
+      # TODO needs to dynamically pull from the recurring schedule
+      interval: 'month'
+    }
+  end
+
   # TODO we need to dedup this
 
   Stripe::Price.create({
@@ -159,7 +256,7 @@ def create_price_for_order_item(sf_order_item)
     # recurring: {interval: 'month'},
 
     metadata: sf_metadata(sf_pricebook_entry)
-  })
+  }.merge(optional_params))
 
   # TODO update ext ID in SF
 end
@@ -169,7 +266,8 @@ def create_subscription_item_from_order_item(sf_order_item)
 end
 
 def recurring_item?(sf_order_item)
-  !sf_order_item.SBQQ__ChargeType__c.nil?
+  # TODO unsure why ChargeType is nil?
+  !sf_order_item.SBQQ__ChargeType__c.nil? || !sf_order_item.SBQQ__SubscriptionType__c.nil?
 end
 
 def create_subscription_from_sf_order(sf_order)
@@ -211,7 +309,12 @@ def create_subscription_from_sf_order(sf_order)
     stripe_transaction = Stripe::Subscription.create({
       customer: stripe_customer,
       items: stripe_prices,
-      metadata: sf_metadata(sf_order)
+      metadata: sf_metadata(sf_order),
+
+      # TODO mainly to avoid having to add a card right now, will need to map thi
+      collection_method: 'send_invoice',
+      days_until_due: 30
+
     })
   else
     stripe_transaction = Stripe::Invoice.create({
