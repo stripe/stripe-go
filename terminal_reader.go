0783c0db228ce0da29d8fadb17b3fe18b3fd2469
Richard Marmorstein <richardm@stripe.com>
2021-10-07 17:53:35 -0400

    Update acronyms

--- terminal_reader.go
+++ terminal_reader.go
@@ -11,8 +11,8 @@ type TerminalReaderDeviceType string
 
 // List of values that TerminalReaderDeviceType can take
 const (
-	TerminalReaderDeviceTypeBBPOSChipper2X TerminalReaderDeviceType = "bbpos_chipper2x"
-	TerminalReaderDeviceTypeBbposWiseposE  TerminalReaderDeviceType = "bbpos_wisepos_e"
+	TerminalReaderDeviceTypeBBPOSChipper2x TerminalReaderDeviceType = "bbpos_chipper2x"
+	TerminalReaderDeviceTypeBBPOSWisePOSE  TerminalReaderDeviceType = "bbpos_wisepos_e"
 	TerminalReaderDeviceTypeVerifoneP400   TerminalReaderDeviceType = "verifone_P400"
 )
 
