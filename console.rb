074f0214d8e9d1addcd831a03854f50b20891184
Michael Bianco <mbianco@stripe.com>
2021-10-14 12:07:36 -0600

    Adding simple console

---
+++ console.rb
@@ -0,0 +1,15 @@
+require_relative 'config'
+
+include StripeForce::Constants
+
+user = StripeForce::User.new(
+  salesforce_token: ENV.fetch('SF_ACCESS_TOKEN'),
+  salesforce_refresh_token: ENV.fetch('SF_REFRESH_TOKEN'),
+  salesforce_instance_url: "https://#{ENV.fetch('SF_INSTANCE')}.my.salesforce.com",
+
+  stripe_account_id: ENV.fetch('STRIPE_ACCOUNT_ID')
+)
+
+sf = user.sf_client
+
+Pry.start
