b182f74c98f50f81f5cee0f6c7b0434bf7d73275
Michael Bianco <mbianco@stripe.com>
2021-10-15 10:33:45 -0600

    Trying to add typing to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# typed: ignore
+# typed: true
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
