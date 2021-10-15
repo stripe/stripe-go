c6ab0fd0acbe1954180d7e15a725bac72f5972bd
Michael Bianco <mbianco@stripe.com>
2021-10-15 10:31:25 -0600

    Fixing omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include SimpleStructuredLogger
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,28 +16,14 @@ class SessionsController < ApplicationController
     redirect_to '/'
   end
 
-  def stripe_callback
-    # TODO check for missing user ID
-    user = StripeForce::User[session[:user_id]]
-    stripe_auth = env['omniauth.auth']
-
-    user.update(
-      stripe_account_id: stripe_auth["uid"],
-      # stripe_refresh_token: stripe_auth[""]
-    )
-
-    erb <<-EOL
-    Great, your orders will now be synced
-    EOL
-  end
-
   def salesforce_callback
-    sf_auth = env['omniauth.auth']
+    sf_auth = auth_hash
     sf_account_id = sf_auth["uid"]
 
     user = StripeForce::User.find(salesforce_account_id: sf_account_id)
 
     if !user
+      log.info 'creating new user', sf_account_id: sf_account_id
       user = StripeForce::User.new(salesforce_account_id: sf_account_id)
     end
 
@@ -46,23 +32,50 @@ class SessionsController < ApplicationController
     sf_instance_url = sf_credentials["instance_url"]
     sf_token = sf_credentials["token"]
 
-    user.salesforce_account_id = sf_account_id,
-                                 user.salesforce_refresh_token = sf_refresh_token,
-                                 user.salesforce_instance_url = sf_instance_url,
-                                 user.salesforce_token = sf_token,
+    user.salesforce_account_id = sf_account_id
+    user.salesforce_refresh_token = sf_refresh_token
+    user.salesforce_instance_url = sf_instance_url
+    user.salesforce_token = sf_token
 
-                                 user.name = sf_auth["extra"]["display_name"],
-                                 user.email = sf_auth["extra"]["email"]
+    user.name = sf_auth["extra"]["display_name"]
+    user.email = sf_auth["extra"]["email"]
 
     user.save
 
     session[:user_id] = user.id
 
-    # puts env['omniauth.auth'].inspect
+    # TODO before redirecting to Stripe, check if there's a valid Stripe connection
 
-    erb <<~EOL
-      <p>SalesForce connected. Let's login to Stripe.</p>
-      <p><a href="/auth/stripe">Connect to Stripe</a>
+    redirect_to '/auth/stripe'
+  end
+
+  def stripe_callback
+    user_id = session[:user_id]
+
+    if user_id.blank?
+      log.warn 'callback requested with empty user id'
+      header :not_found
+      return
+    end
+
+    user = StripeForce::User[user_id]
+    stripe_auth = auth_hash
+    stripe_user_id = stripe_auth["uid"]
+
+    log.info 'updating stripe account ID', user_id: user.id, stripe_account_id: stripe_user_id
+
+    user.update(
+      stripe_account_id: stripe_user_id,
+      # TODO maybe public key?
+      # stripe_refresh_token: stripe_auth[""]
+    )
+
+    render inline: <<-EOL
+    <div style="text-align:center; font-family:'Helvetica Neue', Arial, sans-serif;">
+      <h1 style="margin-top: 30%;">Great, your connected!</h1>
+      <p>Your Stripe & SalesForce accounts are connected. You can safely close this window.</p>
+      <p>Navigate to SalesForce to configure this connector.</p>
+    </div>
     EOL
   end
 
