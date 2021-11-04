1bffed3117ce8822a9665348d7336b52e683ecfe
Michael Bianco <mbianco@stripe.com>
2021-11-03 20:11:01 -0600

    Poll job with error context

--- lib/stripe-force/jobs/initiate_poll_jobs.rb
+++ lib/stripe-force/jobs/initiate_poll_jobs.rb
@@ -1,29 +1,27 @@
 # frozen_string_literal: true
 # typed: true
 class StripeForce::InitiatePollsJobs
-  extend SimpleStructuredLogger
-  # extend StripeForce::ErrorContext
-  # extend StripeForce::ResqueHooks
+  include Integrations::Log
+  extend Integrations::ErrorContext
+  extend Integrations::Metrics::ResqueHooks
 
   @queue = :high
 
   def self.perform
-    # set_error_context
+    set_error_context
 
     log.info 'queuing poll jobs'
 
-    # TODO need to add disabled flag
-    # StripeForce::User.where(disabled: false).extension(:pagination).each_page(50) do |page|
-    StripeForce::User.select_all.extension(:pagination).each_page(50) do |page|
+    StripeForce::User.where(enabled: true).select_all.extension(:pagination).each_page(50) do |page|
       page.each {|user| queue_polls_for_user(user) }
     end
 
-    # set_error_context
+    set_error_context
     log.info 'poll queuing complete'
   end
 
   def self.queue_polls_for_user(user)
-    # set_error_context(user: user)
+    set_error_context(user: user)
 
     # TODO spit out to a separate job
     StripeForce::OrderPoller.perform(user: user)
