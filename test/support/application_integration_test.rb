0a679c78d52a237ca6a7873874e35531818951b4
Michael Bianco <mbianco@stripe.com>
2021-11-04 12:18:31 -0600

    Improving test harness

---
+++ test/support/application_integration_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+# typed: true
+
+class ApplicationIntegrationTest < ActionDispatch::IntegrationTest
+  # include Devise::Test::IntegrationHelpers
+  # include StripeSuiteTestHelpers::Common
+
+  # def setup
+  #   common_setup
+  # end
+
+  # def teardown
+  #   common_teardown
+  # end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,24 @@
 # frozen_string_literal: true
 # typed: strict
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+
+# more information on how the default test runner operates:
+# https://github.com/rails/rails/blob/6-1-stable/railties/lib/rails/test_unit/testing.rake
+# https://github.com/rails/rails/blob/6-1-stable/railties/lib/rails/test_unit/runner.rb
+
 require "minitest/autorun"
-require 'pry-rescue/minitest'
+require "minitest/rails"
+require 'minitest/spec'
+require 'minitest/profile'
+require "mocha/minitest"
+require 'pry-rescue/minitest' unless ENV['CI'] || ENV['EXT_DIR']
+
+Dir[File.join(File.dirname(__FILE__), "test/support/**/*.rb")].sort.each {|f| require f }
+
+Minitest::Ci.clean = false if ENV['CI']
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
