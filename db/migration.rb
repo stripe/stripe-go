b2638d20c6608d8c61fcec66d5053f2a6dfb9775
Michael Bianco <mbianco@stripe.com>
2021-11-03 19:51:43 -0600

    New db schema

--- db/migration.rb
+++ db/migration.rb
@@ -3,10 +3,20 @@
 # heroku run bundle exec ruby migration.rb
 
 require 'sequel'
+
 DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
 
-DB.create_table :users do
+begin
+  DB.drop_table :users
+  DB.drop_table :poll_timestamps
+rescue => e
+end
+
+DB.create_table(:users) do
   primary_key :id
+
+  DateTime :created_at, null: false
+  DateTime :updated_at, null: false
 
   String :salesforce_account_id, unique: true, null: false
   String :salesforce_refresh_token, null: false
@@ -18,7 +28,29 @@ DB.create_table :users do
   String :stripe_public_token
 
   TrueClass :enabled, default: true
+  TrueClass :livemode, default: false
 
   String :name
   String :email
+
+  unique [:stripe_account_id, :salesforce_account_id, :livemode]
+end
+
+DB.create_table(:poll_timestamps) do
+  primary_key :id
+
+  String :stripe_account_id, null: false
+
+  TrueClass :livemode
+
+  # TODO this should be an enum
+  # column :integration_record_type, :netsuite_record_types, null: false
+  String :integration_record_type
+  DateTime :last_polled_at, null: false
+
+  # timestamps
+  DateTime :created_at, null: false
+  DateTime :updated_at, null: false
+
+  # unique [:stripe_account_id, :stripe_account_id, :livemode]
 end
