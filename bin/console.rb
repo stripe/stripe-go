668cfe6b1475a07b9893edff394e19ceb003a45a
Michael Bianco <mbianco@stripe.com>
2021-11-04 17:14:04 -0600

    Fix SF console script

--- bin/console.rb
+++ bin/console.rb
@@ -1,17 +1,34 @@
+#!/usr/bin/env ruby
+
 # frozen_string_literal: true
-# typed: strict
-require_relative 'config'
+# typed: true
+
+require File.expand_path('../config/environment', __dir__)
 
 include StripeForce::Constants
 
-user = StripeForce::User.new(
+user = StripeForce::User.find(salesforce_account_id: ENV.fetch('SF_INSTANCE_ID'))
+
+# most likely, the local creds will be expired
+user ||= StripeForce::User.new(
+  salesforce_account_id: ENV.fetch('SF_INSTANCE_UID'),
   salesforce_token: ENV.fetch('SF_ACCESS_TOKEN'),
   salesforce_refresh_token: ENV.fetch('SF_REFRESH_TOKEN'),
-  salesforce_instance_url: "https://#{ENV.fetch('SF_INSTANCE')}.my.salesforce.com",
+  salesforce_instance_url: "https://#{ENV.fetch('SF_INSTANCE_DOMAIN')}.my.salesforce.com",
 
   stripe_account_id: ENV.fetch('STRIPE_ACCOUNT_ID')
 )
 
-sf = user.sf_client
+@user = user
+@sf = sf = user.sf_client
+
+def example_sf_order
+  # sf.find('Order', '8015e000000IJ1rAAG')
+
+  # order with recurring item
+  # sf.find('Order', '8015e000000IJDxAAO')
+
+  @sf.find('Order', '8015e000000IJF5AAO')
+end
 
 Pry.start
