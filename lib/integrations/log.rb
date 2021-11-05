f15cc541990c2d886d14de62d24034047230f19e
Michael Bianco <mbianco@stripe.com>
2021-11-04 18:59:27 -0600

    Better log formatting, some compatibility hacks

--- lib/integrations/log.rb
+++ lib/integrations/log.rb
@@ -1,8 +1,25 @@
-# typed: strict
+# typed: true
 # frozen_string_literal: true
+
+SimpleStructuredLogger::Writer.class_eval do
+  def level(level=nil)
+    if level
+      @logger.level = level
+    else
+      @logger.level
+    end
+  end
+end
+
+# more simple log formatting
+SimpleStructuredLogger::Writer.instance.logger.formatter = proc do |severity, _datetime, _progname, msg|
+  "#{severity}: #{msg}\n"
+end
 
 module Integrations
   module Log
     include SimpleStructuredLogger
+
+    # TODO handle metrics expansion
   end
 end
