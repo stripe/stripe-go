83630fbbfb9be3ef92aec491314a492ac024e966
Michael Bianco <mbianco@stripe.com>
2021-11-05 13:20:22 -0600

    Resque backtrace cleaner cleanup

--- lib/resque_backtrace.rb
+++ lib/resque_backtrace.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 # typed: true
+
 module Resque
   module Failure
     class Backtrace < Base
       def save
-        # TODO modify once we have the locktime defined here
-        # if exception.class != StripeSuite::Errors::LockTimeout
-        if true
+        if exception.class != Integrations::Errors::LockTimeout
           bt = filter_backtrace(exception.backtrace)
-          SimpleStructuredLogger::Writer.instance.error 'resque exception',
+          Integrations::Log.log.error 'resque exception',
             exception_class: exception.class,
             exception_message: exception.message,
             exception_backtrace: bt.join("\n")
