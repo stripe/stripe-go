115914b808c7b01b0859db6020852f05c154771a
Michael Bianco <mbianco@stripe.com>
2021-11-05 10:21:14 -0600

    Set default values for jsonb

--- lib/stripe-force/db/db.rb
+++ lib/stripe-force/db/db.rb
@@ -1,6 +1,29 @@
 # typed: true
 # frozen_string_literal: true
 
+require 'sequel/plugins/defaults_setter'
+
+# TODO submit a PR for this upstream
+Sequel::Plugins::DefaultsSetter::ClassMethods.class_eval do
+  # Parse the cached database schema for this model and set the default values appropriately.
+  def set_default_values
+    h = {}
+    if @db_schema
+      @db_schema.each do |k, v|
+        if v[:callable_default]
+          h[k] = v[:callable_default]
+        elsif !v[:ruby_default].nil?
+          h[k] = convert_default_value(v[:ruby_default])
+        elsif v[:db_type] == 'jsonb' && v[:default]
+          # extract JSON to use as the initial default value
+          h[k] = v[:default].match(/'(.*)'::jsonb/)[1]
+        end
+      end
+    end
+    @default_values = h.merge!(@default_values || Sequel::OPTS)
+  end
+end
+
 is_test_environment = ENV['RAILS_ENV'] && ENV['RAILS_ENV'] == 'test' && !ENV['TEST_DATABASE_URL'].nil?
 
 url = if is_test_environment
