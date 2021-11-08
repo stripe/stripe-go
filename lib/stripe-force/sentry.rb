b2a72a7b60b2c3230bd25d06227ba5f0101d7edc
Michael Bianco <mbianco@stripe.com>
2021-11-08 15:22:42 -0700

    Improved sentry config

--- lib/stripe-force/sentry.rb
+++ lib/stripe-force/sentry.rb
@@ -1,6 +1,17 @@
 # typed: strict
 # frozen_string_literal: true
 Sentry.init do |config|
+  config.logger = Integrations::Log.log
   config.dsn = ENV.fetch('SENTRY_DSN')
   config.traces_sample_rate = 0.5
+  config.breadcrumbs_logger = [:sentry_logger, :http_logger]
+  config.enabled_environments = %w{production staging}
+  config.excluded_exceptions = []
+
+  # TODO until we can force errors to report sync
+  # https://github.com/getsentry/sentry-ruby/issues/1612
+  config.background_worker_threads = 0
+
+  # `DYNO` is formatted as `worker.12`, `scheduler.1`, etc
+  config.server_name = ENV.fetch('DYNO')[/[^.]+/, 0] if ENV['DYNO']
 end
