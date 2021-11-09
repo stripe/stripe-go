ae160f6e96c17cf43a29d0f4f7cb076dad384398
Michael Bianco <mbianco@stripe.com>
2021-11-09 06:59:33 -0700

    Place test glob config in boot
    fixes use of rails test

--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,9 @@
 # typed: strict
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+# https://github.com/rails/rails/commit/a0f18e60900fc45eb3524ab3cdfe57be430d6016
+ENV['DEFAULT_TEST_EXCLUDE'] = 'test/support/*'
+ENV['DEFAULT_TEST'] = 'test/**/test_*.rb'
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,5 +64,3 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 end
-
-ENV['DEFAULT_TEST_EXCLUDE'] = 'test/support/*'
