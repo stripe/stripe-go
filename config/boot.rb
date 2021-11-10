d6996eeb29ac0822147162f61cba81b0b692eb7c
Michael Bianco <mbianco@stripe.com>
2021-11-10 08:50:00 -0700

    Removing bad test filter filter
    We are naming our tests properly

--- config/boot.rb
+++ config/boot.rb
@@ -4,7 +4,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 # https://github.com/rails/rails/commit/a0f18e60900fc45eb3524ab3cdfe57be430d6016
 ENV['DEFAULT_TEST_EXCLUDE'] = 'test/support/*'
-ENV['DEFAULT_TEST'] = 'test/**/test_*.rb'
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
