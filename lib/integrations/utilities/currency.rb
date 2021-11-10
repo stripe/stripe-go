ae2136562b7536f8f8ec3eadfbb1dd41f8b4ce41
Michael Bianco <mbianco@stripe.com>
2021-11-10 08:53:12 -0700

    Basic harness for base currency detection

--- lib/integrations/utilities/currency.rb
+++ lib/integrations/utilities/currency.rb
@@ -28,8 +28,13 @@ module Integrations::Utilities::Currency
     ZERO_DECIMAL_CURRENCIES.include?(currency_iso.upcase)
   end
 
+  sig { params(string_float_amount: String, user: StripeForce::User).returns(Integer) }
+  def normalize_float_amount_for_stripe(string_float_amount, user)
+    normalize_float_amount_in_currency_for_stripe(base_currency_iso(user), string_float_amount)
+  end
+
   sig { params(currency_iso: String, string_float_amount: String).returns(Integer) }
-  def normalize_float_amount_for_stripe(currency_iso, string_float_amount)
+  def normalize_float_amount_in_currency_for_stripe(currency_iso, string_float_amount)
     if zero_decimal_currency?(currency_iso.upcase)
       string_float_amount.to_i
     else
@@ -37,6 +42,13 @@ module Integrations::Utilities::Currency
       float_amount = BigDecimal(string_float_amount)
       (float_amount * 100.0).to_i
     end
+  end
+
+  sig { params(user: StripeForce::User).returns(String) }
+  def base_currency_iso(user)
+    # TODO should cache result from Stripe?
+    # TODO determine currency from SF?
+    'usd'
   end
 
 end
