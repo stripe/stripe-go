d64421dfa0917b0c9e8c23e9e3bd76bb60e676b8
Michael Bianco <mbianco@stripe.com>
2021-11-11 12:29:40 -0700

    Adding spring db disconnect

--- config/spring.rb
+++ config/spring.rb
@@ -4,5 +4,11 @@ Spring.watch(
   ".ruby-version",
   ".rbenv-vars",
   "tmp/restart.txt",
-  "tmp/caching-dev.txt"
+  "tmp/caching-dev.txt",
+
 )
+
+# https://github.com/TalentBox/sequel-rails/commit/e3e35209ee45918ce1f61daa6ef7850c881fac24
+Spring.after_fork do
+  Sequel::DATABASES.each {|db| db.disconnect }
+end
