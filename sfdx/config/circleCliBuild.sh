bc021de0498737734c388769174477feec8be70f
arnoldezeolisa <arnold.ezeolisa@gmail.com>
2021-11-21 01:40:50 -0600

    working on adding circleci

---
+++ sfdx/config/circleCliBuild.sh
@@ -0,0 +1,1 @@
+sfdx auth:jwt:grant --clientid 3MVG9_7ddP9KqTzco5f8yk9A4U6BZW4SbOYWnBpGFyCu.8SSXLAZASV.Y4Iq4dredXg78NiRh4.Gkwltu05vw --jwtkeyfile /Users/arnoldezeolisa/Dropbox/Projects/stripe-salesforce/sfdx/JWT/server.key --username test-6c7dn5uulbli@example.com --instanceurl https://test.salesforce.com
