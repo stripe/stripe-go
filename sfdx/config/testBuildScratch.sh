b6e88e17a42f9e1d3cb85c9d3c2062169d374f36
arnoldezeolisa <arnold.ezeolisa@gmail.com>
2021-11-22 22:38:51 -0600

    reverting SF circleCI stuff

--- sfdx/config/testBuildScratch.sh
+++
@@ -1,6 +0,0 @@
-
-echo "Connect to Dev Hub Og"
-sfdx force:auth:web:login -d -a "stripescratch"
-echo "Log in"
-sfdx force:config:set defaultdevhubusername=test-6c7dn5uulbli@example.com
-/Users/arnoldezeolisa/Dropbox/Projects/stripe-salesforce/sfdx/config/circleCliBuild.sh
