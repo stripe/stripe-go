4b0544934989b85c78150ebc3b8e01a9ccbcab82
Michael Bianco <mbianco@stripe.com>
2021-12-06 10:42:27 -0700

    Another postmessage fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -91,8 +91,7 @@ class SessionsController < ApplicationController
       <p>Navigate to SalesForce to configure this connector.</p>
     </div>
     <script type="application/javascript">
-    // window.opener.parent.postMessage("connectionSuccessful", "https://#{user.sf_subdomain}.lightning.force.com")
-    window.opener.postMessage("connectionSuccessful", "https://#{user.sf_subdomain}.visualforce.com")
+    window.opener.postMessage("connectionSuccessful", "https://#{user.sf_subdomain}--c.visualforce.com")
     </script>
     EOL
   end
