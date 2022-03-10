8bb264352c5063c5cb58fa4a8945ea57ffdd0d48
Michael Bianco <mbianco@stripe.com>
2022-03-10 15:05:57 -0700

    Grab bag of minor tweaks

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :stripe, ENV["STRIPE_CLIENT_ID"], ENV["STRIPE_CLIENT_SECRET"], scope: 'read_write'
 
-  provider :salesforce, ENV['SF_CONSUMER_KEY'], ENV['SF_CONSUMER_SECRET']
+  provider :salesforce, ENV.fetch('SF_CONSUMER_KEY'), ENV.fetch('SF_CONSUMER_SECRET')
 
   provider OmniAuth::Strategies::SalesforceSandbox,
     ENV['SF_CONSUMER_KEY'],
