1b7903bfa70d104f77010751e84a5cef95a5f2d6
Dominic Charley-Roy <78050200+dcr-stripe@users.noreply.github.com>
2022-04-04 09:11:41 -0400

    Add support for passing expansions in SearchParams. (#1443)

---
+++ search_params_test.go
@@ -0,0 +1,80 @@
+package stripe_test
+
+import (
+	"context"
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	stripe "github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/form"
+	. "github.com/stripe/stripe-go/v72/testing"
+)
+
+type testSearchParams struct {
+	stripe.SearchParams `form:"*"`
+	Page                *string `form:"page"`
+}
+
+func TestSearchParams_Nested(t *testing.T) {
+	params := &testSearchParams{
+		Page: stripe.String("page_value"),
+		SearchParams: stripe.SearchParams{
+			Query: "query_value",
+		},
+	}
+
+	body := &form.Values{}
+	form.AppendTo(body, params)
+
+	assert.Equal(t, valuesFromArray([][2]string{
+		{"query", "query_value"},
+		{"page", "page_value"},
+	}), body)
+}
+
+func TestSearchParams_Expand(t *testing.T) {
+	testCases := []struct {
+		InitialBody  [][2]string
+		Expand       []string
+		ExpectedBody [][2]string
+	}{
+		{
+			InitialBody:  [][2]string{{"foo", "bar"}},
+			Expand:       []string{},
+			ExpectedBody: [][2]string{{"foo", "bar"}},
+		},
+		{
+			InitialBody:  [][2]string{{"foo", "bar"}, {"foo", "baz"}},
+			Expand:       []string{"data", "data.foo"},
+			ExpectedBody: [][2]string{{"foo", "bar"}, {"foo", "baz"}, {"expand[0]", "data"}, {"expand[1]", "data.foo"}},
+		},
+	}
+
+	for _, testCase := range testCases {
+		p := stripe.SearchParams{}
+
+		for _, exp := range testCase.Expand {
+			p.AddExpand(exp)
+		}
+
+		body := valuesFromArray(testCase.InitialBody)
+		form.AppendTo(body, p)
+		assert.Equal(t, valuesFromArray(testCase.ExpectedBody), body)
+	}
+}
+
+func TestSearchParams_SetStripeAccount(t *testing.T) {
+	p := &stripe.SearchParams{}
+	p.SetStripeAccount(TestMerchantID)
+	assert.Equal(t, TestMerchantID, *p.StripeAccount)
+}
+
+func TestSearchParams_ToParams(t *testing.T) {
+	SearchParams := &stripe.SearchParams{
+		Context: context.Background(),
+	}
+	SearchParams.SetStripeAccount(TestMerchantID)
+	params := SearchParams.ToParams()
+	assert.Equal(t, SearchParams.Context, params.Context)
+	assert.Equal(t, *SearchParams.StripeAccount, *params.StripeAccount)
+}
