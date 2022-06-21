f863cf857edc314cc680de8f1bf0b3c5a89ada22
Pavel Krymets <pakrym@stripe.com>
2022-06-21 14:09:21 -0700

    update the file

--- api_version.go
+++ api_version.go
@@ -9,3 +9,8 @@ package stripe
 const (
 	apiVersion string = "2020-08-27"
 )
+
+//
+// Suppress the unused constant warning
+// The constant is not used for GA releases but is used in betas
+var _ = apiVersion
