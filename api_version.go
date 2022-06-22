0653ba53bef290131a362b82e83676cee81743f6
Pavel Krymets <pakrym@stripe.com>
2022-06-22 13:16:08 -0700

    Use the generated API version 2

---
+++ api_version.go
@@ -0,0 +1,16 @@
+//
+//
+// File generated from our OpenAPI spec
+//
+//
+
+package stripe
+
+const (
+	apiVersion string = "2020-08-27"
+)
+
+//
+// Suppress the unused constant warning
+// The constant is not used for GA releases but is used in betas
+var _ = apiVersion
