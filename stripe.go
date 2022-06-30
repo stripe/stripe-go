b493e838a463c7b9874c751e53c6f54f4cddd58c
Pavel Krymets <pakrym@stripe.com>
2022-06-30 13:08:14 -0700

    Set version to 72.117.0 to simplify merge

--- stripe.go
+++ stripe.go
@@ -1203,7 +1203,7 @@ func StringSlice(v []string) []*string {
 //
 
 // clientversion is the binding version
-const clientversion = "72.115.0-beta.1"
+const clientversion = "72.117.0"
 
 // defaultHTTPTimeout is the default timeout on the http.Client used by the library.
 // This is chosen to be consistent with the other Stripe language libraries and
