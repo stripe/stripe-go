4cf122e3d7c8e2bc94aaabbfec83bff6dcbb0a76
Yejia Chen <yejia@stripe.com>
2022-07-12 15:17:04 -0400

    Codegen for openapi v164

--- checkout_session.go
+++ checkout_session.go
@@ -501,11 +501,19 @@ const (
 	CheckoutSessionSubmitTypePay    CheckoutSessionSubmitType = "pay"
 )
 
+// Only return the Checkout Sessions for the Customer details specified.
+type CheckoutSessionListCustomerDetailsParams struct {
+	// Customer's email address.
+	Email *string `form:"email"`
+}
+
 // Returns a list of Checkout Sessions.
 type CheckoutSessionListParams struct {
 	ListParams `form:"*"`
 	// Only return the Checkout Sessions for the Customer specified.
 	Customer *string `form:"customer"`
+	// Only return the Checkout Sessions for the Customer details specified.
+	CustomerDetails *CheckoutSessionListCustomerDetailsParams `form:"customer_details"`
 	// Only return the Checkout Session for the PaymentIntent specified.
 	PaymentIntent *string `form:"payment_intent"`
 	// Only return the Checkout Session for the subscription specified.
