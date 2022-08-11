5a914f1fdcb8cef7d9c3eed66558bf7672543c40
Pavel Krymets <pakrym@stripe.com>
2022-08-11 14:51:43 -0700

    Reset version to 73.1.0-beta.1

--- stripe.go
+++ stripe.go
@@ -1198,7 +1198,7 @@ func StringSlice(v []string) []*string {
 //
 
 // clientversion is the binding version
-const clientversion = "73.2.0"
+const clientversion = "73.1.0-beta.1"
 
 // defaultHTTPTimeout is the default timeout on the http.Client used by the library.
 // This is chosen to be consistent with the other Stripe language libraries and
