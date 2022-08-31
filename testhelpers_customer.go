bfc88f6f5247a50cfa2eb42e1b6837e1807ca3c2
pakrym-stripe <99349468+pakrym-stripe@users.noreply.github.com>
2022-08-31 08:36:36 -0700

    API Updates (#1541)
    Codegen for openapi v188

--- testhelpers_customer.go
+++ testhelpers_customer.go
@@ -9,9 +9,10 @@ package stripe
 // Create an incoming testmode bank transfer
 type TestHelpersCustomerFundCashBalanceParams struct {
 	Params `form:"*"`
-	// Amount intended to be collected by this PaymentIntent. A positive integer representing how much to charge in the [smallest currency unit](https://stripe.com/docs/currencies#zero-decimal) (e.g., 100 cents to charge $1.00 or 100 to charge ¥100, a zero-decimal currency). The minimum amount is $0.50 US or [equivalent in charge currency](https://stripe.com/docs/currencies#minimum-and-maximum-charge-amounts). The amount value supports up to eight digits (e.g., a value of 99999999 for a USD charge of $999,999.99).
+	// Amount to be used for this test cash balance transaction. A positive integer representing how much to fund in the [smallest currency unit](https://stripe.com/docs/currencies#zero-decimal) (e.g., 100 cents to fund $1.00 or 100 to fund ¥100, a zero-decimal currency).
 	Amount *int64 `form:"amount"`
 	// Three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html), in lowercase. Must be a [supported currency](https://stripe.com/docs/currencies).
-	Currency  *string `form:"currency"`
+	Currency *string `form:"currency"`
+	// A description of the test funding. This simulates free-text references supplied by customers when making bank transfers to their cash balance. You can use this to test how Stripe's [reconciliation algorithm](https://stripe.com/docs/payments/customer-balance/reconciliation) applies to different user inputs.
 	Reference *string `form:"reference"`
 }
