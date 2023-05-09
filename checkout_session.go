d5cc4640f532577349b13adf1668e6fbe6c1b29d
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-09 10:20:22 +0000

    Update generated code for v330

--- checkout_session.go
+++ checkout_session.go
@@ -1229,8 +1229,20 @@ type CheckoutSessionPaymentMethodOptionsPayNowParams struct {
 
 // contains details about the PayPal payment method options.
 type CheckoutSessionPaymentMethodOptionsPaypalParams struct {
-	BillingAgreementID *string `form:"billing_agreement_id"`
-	Currency           *string `form:"currency"`
+	CaptureMethod   *string `form:"capture_method"`
+	PreferredLocale *string `form:"preferred_locale"`
+	Reference       *string `form:"reference"`
+	ReferenceID     *string `form:"reference_id"`
+	// The risk correlation ID for an on-session payment using a saved PayPal payment method.
+	RiskCorrelationID *string `form:"risk_correlation_id"`
+	// Indicates that you intend to make future payments with this PaymentIntent's payment method.
+	//
+	// Providing this parameter will [attach the payment method](https://stripe.com/docs/payments/save-during-payment) to the PaymentIntent's Customer, if present, after the PaymentIntent is confirmed and any required actions from the user are complete. If no Customer was provided, the payment method can still be [attached](https://stripe.com/docs/api/payment_methods/attach) to a Customer after the transaction completes.
+	//
+	// When processing card payments, Stripe also uses `setup_future_usage` to dynamically optimize your payment flow and comply with regional legislation and network rules, such as [SCA](https://stripe.com/docs/strong-customer-authentication).
+	//
+	// If `setup_future_usage` is already set and you are performing a request using a publishable key, you may only update the value from `on_session` to `off_session`.
+	SetupFutureUsage *string `form:"setup_future_usage"`
 }
 
 // contains details about the Pix payment method options.
