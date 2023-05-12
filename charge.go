d1cfd33fa0c6aa6222a1aeb8935ee68cc191aadf
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-12 14:05:35 +0000

    Update generated code for v340

--- charge.go
+++ charge.go
@@ -749,7 +749,12 @@ type ChargePaymentMethodDetailsCardPresent struct {
 	// The name of the card's issuing bank. (For internal use only and not typically available in standard API requests.)
 	Issuer string `json:"issuer"`
 }
-type ChargePaymentMethodDetailsCashApp struct{}
+type ChargePaymentMethodDetailsCashApp struct {
+	// A unique and immutable identifier assigned by Cash App to every buyer.
+	BuyerID string `json:"buyer_id"`
+	// A public identifier for buyers using Cash App.
+	Cashtag string `json:"cashtag"`
+}
 type ChargePaymentMethodDetailsCustomerBalance struct{}
 type ChargePaymentMethodDetailsEPS struct {
 	// The customer's bank. Should be one of `arzte_und_apotheker_bank`, `austrian_anadi_bank_ag`, `bank_austria`, `bankhaus_carl_spangler`, `bankhaus_schelhammer_und_schattera_ag`, `bawag_psk_ag`, `bks_bank_ag`, `brull_kallmus_bank_ag`, `btv_vier_lander_bank`, `capital_bank_grawe_gruppe_ag`, `deutsche_bank_ag`, `dolomitenbank`, `easybank_ag`, `erste_bank_und_sparkassen`, `hypo_alpeadriabank_international_ag`, `hypo_noe_lb_fur_niederosterreich_u_wien`, `hypo_oberosterreich_salzburg_steiermark`, `hypo_tirol_bank_ag`, `hypo_vorarlberg_bank_ag`, `hypo_bank_burgenland_aktiengesellschaft`, `marchfelder_bank`, `oberbank_ag`, `raiffeisen_bankengruppe_osterreich`, `schoellerbank_ag`, `sparda_bank_wien`, `volksbank_gruppe`, `volkskreditbank_ag`, or `vr_bank_braunau`.
