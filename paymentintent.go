808f6aed480444499ea07d5e691d708f76520412
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-18 20:03:30 +0000

    Update generated code for v349

--- paymentintent.go
+++ paymentintent.go
@@ -232,6 +232,26 @@ const (
 	PaymentIntentPaymentMethodOptionsBancontactSetupFutureUsageOffSession PaymentIntentPaymentMethodOptionsBancontactSetupFutureUsage = "off_session"
 )
 
+// Frequency interval of each recurring payment.
+type PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionInterval string
+
+// List of values that PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionInterval can take
+const (
+	PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionIntervalDay   PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionInterval = "day"
+	PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionIntervalMonth PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionInterval = "month"
+	PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionIntervalWeek  PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionInterval = "week"
+	PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionIntervalYear  PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionInterval = "year"
+)
+
+// Type of the mandate.
+type PaymentIntentPaymentMethodOptionsBLIKMandateOptionsType string
+
+// List of values that PaymentIntentPaymentMethodOptionsBLIKMandateOptionsType can take
+const (
+	PaymentIntentPaymentMethodOptionsBLIKMandateOptionsTypeOffSession PaymentIntentPaymentMethodOptionsBLIKMandateOptionsType = "off_session"
+	PaymentIntentPaymentMethodOptionsBLIKMandateOptionsTypeOnSession  PaymentIntentPaymentMethodOptionsBLIKMandateOptionsType = "on_session"
+)
+
 // Indicates that you intend to make future payments with this PaymentIntent's payment method.
 //
 // Providing this parameter will [attach the payment method](https://stripe.com/docs/payments/save-during-payment) to the PaymentIntent's Customer, if present, after the PaymentIntent is confirmed and any required actions from the user are complete. If no Customer was provided, the payment method can still be [attached](https://stripe.com/docs/api/payment_methods/attach) to a Customer after the transaction completes.
@@ -1025,10 +1045,28 @@ type PaymentIntentPaymentMethodOptionsBancontactParams struct {
 	SetupFutureUsage *string `form:"setup_future_usage"`
 }
 
+// Details of off-session mandate.
+type PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionParams struct {
+	// Frequency interval of each recurring payment.
+	Interval *string `form:"interval"`
+	// Frequency indicator of each recurring payment.
+	IntervalCount *int64 `form:"interval_count"`
+}
+
+// Details of the BLIK mandate
+type PaymentIntentPaymentMethodOptionsBLIKMandateOptionsParams struct {
+	// Expiry date of the mandate.
+	ExpiresAfter *int64 `form:"expires_after"`
+	// Details of off-session mandate.
+	OffSession *PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionParams `form:"off_session"`
+}
+
 // If this is a `blik` PaymentMethod, this sub-hash contains details about the BLIK payment method options.
 type PaymentIntentPaymentMethodOptionsBLIKParams struct {
 	// The 6-digit BLIK code that a customer has generated using their banking application. Can only be set on confirmation.
 	Code *string `form:"code"`
+	// Details of the BLIK mandate
+	MandateOptions *PaymentIntentPaymentMethodOptionsBLIKMandateOptionsParams `form:"mandate_options"`
 }
 
 // If this is a `boleto` PaymentMethod, this sub-hash contains details about the Boleto payment method options.
@@ -2206,7 +2244,26 @@ type PaymentIntentPaymentMethodOptionsBancontact struct {
 	// When processing card payments, Stripe also uses `setup_future_usage` to dynamically optimize your payment flow and comply with regional legislation and network rules, such as [SCA](https://stripe.com/docs/strong-customer-authentication).
 	SetupFutureUsage PaymentIntentPaymentMethodOptionsBancontactSetupFutureUsage `json:"setup_future_usage"`
 }
-type PaymentIntentPaymentMethodOptionsBLIK struct{}
+type PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSession struct {
+	// Amount of each recurring payment.
+	Amount int64 `json:"amount"`
+	// Currency of each recurring payment.
+	Currency Currency `json:"currency"`
+	// Frequency interval of each recurring payment.
+	Interval PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSessionInterval `json:"interval"`
+	// Frequency indicator of each recurring payment.
+	IntervalCount int64 `json:"interval_count"`
+}
+type PaymentIntentPaymentMethodOptionsBLIKMandateOptions struct {
+	// Date at which the mandate expires.
+	ExpiresAfter int64                                                          `json:"expires_after"`
+	OffSession   *PaymentIntentPaymentMethodOptionsBLIKMandateOptionsOffSession `json:"off_session"`
+	// Type of the mandate.
+	Type PaymentIntentPaymentMethodOptionsBLIKMandateOptionsType `json:"type"`
+}
+type PaymentIntentPaymentMethodOptionsBLIK struct {
+	MandateOptions *PaymentIntentPaymentMethodOptionsBLIKMandateOptions `json:"mandate_options"`
+}
 type PaymentIntentPaymentMethodOptionsBoleto struct {
 	// The number of calendar days before a Boleto voucher expires. For example, if you create a Boleto voucher on Monday and you set expires_after_days to 2, the Boleto voucher will expire on Wednesday at 23:59 America/Sao_Paulo time.
 	ExpiresAfterDays int64 `json:"expires_after_days"`
