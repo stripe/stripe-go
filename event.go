a87582604c3d569300a0c8e880edfb57a10d1356
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-06-06 18:00:33 +0000

    Update generated code for v375

--- event.go
+++ event.go
@@ -35,7 +35,7 @@ type EventData struct {
 	// a custom UnmarshalJSON implementation.
 	// Object containing the API resource relevant to the event. For example, an `invoice.created` event will have a full [invoice object](https://stripe.com/docs/api#invoice_object) as the value of the object key.
 	Object map[string]interface{} `json:"-"`
-	// Object containing the names of the attributes that have changed, and their previous values (sent along only with *.updated events).
+	// Object containing the names of the updated attributes and their values prior to the event (only included in events of type `*.updated`). If an array attribute has any updated elements, this object contains the entire array. In Stripe API versions 2017-04-06 or earlier, an updated array attribute in this object includes only the updated array elements.
 	PreviousAttributes map[string]interface{} `json:"previous_attributes"`
 	Raw                json.RawMessage        `json:"object"`
 }
