a5c6b63fec9ae6203d413f0b9241f3afbdb7b489
jmather-c <117302272+jmather-c@users.noreply.github.com>
2023-11-16 10:14:58 -0800

    Fix customer label changing to account (#1248)

--- sfdx/force-app/main/default/lwc/dataMappingStep/dataMappingStep.js
+++ sfdx/force-app/main/default/lwc/dataMappingStep/dataMappingStep.js
@@ -69,7 +69,7 @@ export default class DataMappingStep extends LightningElement {
     ACTIVE_OBJECT_INFO = {
         "customer": {
             objectName: "Account",
-            friendlyName: 'Account',
+            friendlyName: 'Customer',
             description: 'Customer objects allow you to perform recurring charges, and to track multiple charges, that are associated with the same customer. The API allows you to create, delete, and update your customers. You can retrieve individual customers as well as a list of all your customers.'
         },
         "subscriptionSchedule": {
