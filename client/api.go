3b855f4895d6e23fb116f0476b07bcbaa27ed722
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-22 01:00:21 +0000

    Update generated code for v682

--- client/api.go
+++ client/api.go
@@ -25,6 +25,9 @@ import (
 	"github.com/stripe/stripe-go/v76/cashbalance"
 	"github.com/stripe/stripe-go/v76/charge"
 	checkoutsession "github.com/stripe/stripe-go/v76/checkout/session"
+	climateorder "github.com/stripe/stripe-go/v76/climate/order"
+	climateproduct "github.com/stripe/stripe-go/v76/climate/product"
+	climatesupplier "github.com/stripe/stripe-go/v76/climate/supplier"
 	"github.com/stripe/stripe-go/v76/countryspec"
 	"github.com/stripe/stripe-go/v76/coupon"
 	"github.com/stripe/stripe-go/v76/creditnote"
@@ -157,6 +160,12 @@ type API struct {
 	Charges *charge.Client
 	// CheckoutSessions is the client used to invoke /checkout/sessions APIs.
 	CheckoutSessions *checkoutsession.Client
+	// ClimateOrders is the client used to invoke /climate/orders APIs.
+	ClimateOrders *climateorder.Client
+	// ClimateProducts is the client used to invoke /climate/products APIs.
+	ClimateProducts *climateproduct.Client
+	// ClimateSuppliers is the client used to invoke /climate/suppliers APIs.
+	ClimateSuppliers *climatesupplier.Client
 	// CountrySpecs is the client used to invoke /country_specs APIs.
 	CountrySpecs *countryspec.Client
 	// Coupons is the client used to invoke /coupons APIs.
@@ -377,6 +386,9 @@ func (a *API) Init(key string, backends *stripe.Backends) {
 	a.CashBalances = &cashbalance.Client{B: backends.API, Key: key}
 	a.Charges = &charge.Client{B: backends.API, Key: key}
 	a.CheckoutSessions = &checkoutsession.Client{B: backends.API, Key: key}
+	a.ClimateOrders = &climateorder.Client{B: backends.API, Key: key}
+	a.ClimateProducts = &climateproduct.Client{B: backends.API, Key: key}
+	a.ClimateSuppliers = &climatesupplier.Client{B: backends.API, Key: key}
 	a.CountrySpecs = &countryspec.Client{B: backends.API, Key: key}
 	a.Coupons = &coupon.Client{B: backends.API, Key: key}
 	a.CreditNotes = &creditnote.Client{B: backends.API, Key: key}
---
+++ climate/supplier/client.go
@@ -0,0 +1,77 @@
+//
+//
+// File generated from our OpenAPI spec
+//
+//
+
+// Package supplier provides the /climate/suppliers APIs
+package supplier
+
+import (
+	"net/http"
+
+	stripe "github.com/stripe/stripe-go/v76"
+	"github.com/stripe/stripe-go/v76/form"
+)
+
+// Client is used to invoke /climate/suppliers APIs.
+type Client struct {
+	B   stripe.Backend
+	Key string
+}
+
+// Get returns the details of a climate supplier.
+func Get(id string, params *stripe.ClimateSupplierParams) (*stripe.ClimateSupplier, error) {
+	return getC().Get(id, params)
+}
+
+// Get returns the details of a climate supplier.
+func (c Client) Get(id string, params *stripe.ClimateSupplierParams) (*stripe.ClimateSupplier, error) {
+	path := stripe.FormatURLPath("/v1/climate/suppliers/%s", id)
+	supplier := &stripe.ClimateSupplier{}
+	err := c.B.Call(http.MethodGet, path, c.Key, params, supplier)
+	return supplier, err
+}
+
+// List returns a list of climate suppliers.
+func List(params *stripe.ClimateSupplierListParams) *Iter {
+	return getC().List(params)
+}
+
+// List returns a list of climate suppliers.
+func (c Client) List(listParams *stripe.ClimateSupplierListParams) *Iter {
+	return &Iter{
+		Iter: stripe.GetIter(listParams, func(p *stripe.Params, b *form.Values) ([]interface{}, stripe.ListContainer, error) {
+			list := &stripe.ClimateSupplierList{}
+			err := c.B.CallRaw(http.MethodGet, "/v1/climate/suppliers", c.Key, b, p, list)
+
+			ret := make([]interface{}, len(list.Data))
+			for i, v := range list.Data {
+				ret[i] = v
+			}
+
+			return ret, list, err
+		}),
+	}
+}
+
+// Iter is an iterator for climate suppliers.
+type Iter struct {
+	*stripe.Iter
+}
+
+// ClimateSupplier returns the climate supplier which the iterator is currently pointing to.
+func (i *Iter) ClimateSupplier() *stripe.ClimateSupplier {
+	return i.Current().(*stripe.ClimateSupplier)
+}
+
+// ClimateSupplierList returns the current list object which the iterator is
+// currently using. List objects will change as new API calls are made to
+// continue pagination.
+func (i *Iter) ClimateSupplierList() *stripe.ClimateSupplierList {
+	return i.List().(*stripe.ClimateSupplierList)
+}
+
+func getC() Client {
+	return Client{stripe.GetBackend(stripe.APIBackend), stripe.Key}
+}
