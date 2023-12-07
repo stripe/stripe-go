2dc80757db8689ad01cef50d7a2c7d81873bcbdf
Pavel Krymets <pakrym@stripe.com>
2023-12-07 13:12:48 -0800

    Bump version to 76.8.0

--- stripe.go
+++ stripe.go
@@ -1226,7 +1226,7 @@ func StringSlice(v []string) []*string {
 //
 
 // clientversion is the binding version
-const clientversion = "76.7.0"
+const clientversion = "76.8.0"
 
 // defaultHTTPTimeout is the default timeout on the http.Client used by the library.
 // This is chosen to be consistent with the other Stripe language libraries and
