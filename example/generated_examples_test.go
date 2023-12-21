ba5de9fb3365eb4455907a0c2124dc9ae101a4a3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-12-21 19:42:51 +0000

    Update generated code for v733

--- example/generated_examples_test.go
+++ example/generated_examples_test.go
@@ -72,6 +72,7 @@ import (
 	subscriptionschedule "github.com/stripe/stripe-go/v76/subscriptionschedule"
 	tax_calculation "github.com/stripe/stripe-go/v76/tax/calculation"
 	tax_form "github.com/stripe/stripe-go/v76/tax/form"
+	tax_settings "github.com/stripe/stripe-go/v76/tax/settings"
 	tax_transaction "github.com/stripe/stripe-go/v76/tax/transaction"
 	taxcode "github.com/stripe/stripe-go/v76/taxcode"
 	taxid "github.com/stripe/stripe-go/v76/taxid"
@@ -2718,6 +2719,24 @@ func TestTaxRatesPost(t *testing.T) {
 func TestTaxRatesPost2(t *testing.T) {
 	params := &stripe.TaxRateParams{Active: stripe.Bool(false)}
 	result, err := taxrate.Update("txr_xxxxxxxxxxxxx", params)
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestTaxSettingsGet(t *testing.T) {
+	params := &stripe.TaxSettingsParams{}
+	result, err := tax_settings.Get(params)
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestTaxSettingsPost(t *testing.T) {
+	params := &stripe.TaxSettingsParams{
+		Defaults: &stripe.TaxSettingsDefaultsParams{
+			TaxCode: stripe.String("txcd_10000000"),
+		},
+	}
+	result, err := tax_settings.Update(params)
 	assert.NotNil(t, result)
 	assert.Nil(t, err)
 }
