4724a722f4ffbcc740b5231b825489be1c37cb69
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-06 22:54:33 +0000

    Update generated code for v866

--- invoice.go
+++ invoice.go
@@ -4495,7 +4495,8 @@ type InvoiceSubscriptionDetailsPauseCollection struct {
 
 // Details about the subscription that created this invoice.
 type InvoiceSubscriptionDetails struct {
-	// Set of [key-value pairs](https://stripe.com/docs/api/metadata) that will reflect the metadata of the subscription at the time of invoice creation. *Note: This attribute is populated only for invoices created on or after June 29, 2023.*
+	// Set of [key-value pairs](https://stripe.com/docs/api/metadata) defined as subscription metadata when an invoice is created. Becomes an immutable snapshot of the subscription metadata at the time of invoice finalization.
+	//  *Note: This attribute is populated only for invoices created on or after June 29, 2023.*
 	Metadata map[string]string `json:"metadata"`
 	// If specified, payment collection for this subscription will be paused.
 	PauseCollection *InvoiceSubscriptionDetailsPauseCollection `json:"pause_collection"`
