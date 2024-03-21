a45a54fd1bc32a42c58529728fd42aa01ab0aa57
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-21 20:50:44 +0000

    Update generated code for v896

--- quote.go
+++ quote.go
@@ -1302,7 +1302,7 @@ func (p *QuoteReestimateParams) AddExpand(f string) {
 	p.Expand = append(p.Expand, &f)
 }
 
-// Download the PDF for a finalized quote
+// Download the PDF for a finalized quote. Explanation for special handling can be found [here](https://docs.corp.stripe.com/quotes/overview#quote_pdf)
 type QuotePDFParams struct {
 	Params `form:"*"`
 	// Specifies which fields in the response should be expanded.
