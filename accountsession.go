9fc734d8297249225d6d273ebcf069d9b3d90ed1
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-25 19:15:02 +0000

    Update generated code for v903

--- accountsession.go
+++ accountsession.go
@@ -41,6 +41,8 @@ type AccountSessionComponentsDocumentsParams struct {
 type AccountSessionComponentsPaymentDetailsFeaturesParams struct {
 	// Whether to allow capturing and cancelling payment intents. This is `true` by default.
 	CapturePayments *bool `form:"capture_payments"`
+	// Whether to allow connected accounts to manage destination charges that are created on behalf of them. This is `false` by default.
+	DestinationOnBehalfOfChargeManagement *bool `form:"destination_on_behalf_of_charge_management"`
 	// Whether to allow responding to disputes, including submitting evidence and accepting disputes. This is `true` by default.
 	DisputeManagement *bool `form:"dispute_management"`
 	// Whether to allow sending refunds. This is `true` by default.
@@ -59,6 +61,8 @@ type AccountSessionComponentsPaymentDetailsParams struct {
 type AccountSessionComponentsPaymentsFeaturesParams struct {
 	// Whether to allow capturing and cancelling payment intents. This is `true` by default.
 	CapturePayments *bool `form:"capture_payments"`
+	// Whether to allow connected accounts to manage destination charges that are created on behalf of them. This is `false` by default.
+	DestinationOnBehalfOfChargeManagement *bool `form:"destination_on_behalf_of_charge_management"`
 	// Whether to allow responding to disputes, including submitting evidence and accepting disputes. This is `true` by default.
 	DisputeManagement *bool `form:"dispute_management"`
 	// Whether to allow sending refunds. This is `true` by default.
@@ -143,6 +147,8 @@ type AccountSessionComponentsDocuments struct {
 type AccountSessionComponentsPaymentDetailsFeatures struct {
 	// Whether to allow capturing and cancelling payment intents. This is `true` by default.
 	CapturePayments bool `json:"capture_payments"`
+	// Whether to allow connected accounts to manage destination charges that are created on behalf of them. This is `false` by default.
+	DestinationOnBehalfOfChargeManagement bool `json:"destination_on_behalf_of_charge_management"`
 	// Whether to allow responding to disputes, including submitting evidence and accepting disputes. This is `true` by default.
 	DisputeManagement bool `json:"dispute_management"`
 	// Whether to allow sending refunds. This is `true` by default.
@@ -156,6 +162,8 @@ type AccountSessionComponentsPaymentDetails struct {
 type AccountSessionComponentsPaymentsFeatures struct {
 	// Whether to allow capturing and cancelling payment intents. This is `true` by default.
 	CapturePayments bool `json:"capture_payments"`
+	// Whether to allow connected accounts to manage destination charges that are created on behalf of them. This is `false` by default.
+	DestinationOnBehalfOfChargeManagement bool `json:"destination_on_behalf_of_charge_management"`
 	// Whether to allow responding to disputes, including submitting evidence and accepting disputes. This is `true` by default.
 	DisputeManagement bool `json:"dispute_management"`
 	// Whether to allow sending refunds. This is `true` by default.
