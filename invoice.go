67845ae49d8ae819ca88ad9d05c795a1532056bb
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-26 15:45:35 +0000

    Update generated code for v996

--- invoice.go
+++ invoice.go
@@ -4929,9 +4929,9 @@ type Invoice struct {
 	// Payments for this invoice
 	Payments        *InvoicePaymentList     `json:"payments"`
 	PaymentSettings *InvoicePaymentSettings `json:"payment_settings"`
-	// End of the usage period during which invoice items were added to this invoice.
+	// End of the usage period during which invoice items were added to this invoice. This looks back one period for a subscription invoice. Use the [line item period](https://stripe.com/api/invoices/line_item#invoice_line_item_object-period) to get the service period for each price.
 	PeriodEnd int64 `json:"period_end"`
-	// Start of the usage period during which invoice items were added to this invoice.
+	// Start of the usage period during which invoice items were added to this invoice. This looks back one period for a subscription invoice. Use the [line item period](https://stripe.com/api/invoices/line_item#invoice_line_item_object-period) to get the service period for each price.
 	PeriodStart int64 `json:"period_start"`
 	// Total amount of all post-payment credit notes issued for this invoice.
 	PostPaymentCreditNotesAmount int64 `json:"post_payment_credit_notes_amount"`
