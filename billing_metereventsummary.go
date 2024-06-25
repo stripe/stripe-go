8dee52b118765c381cf4dc8c8fba414ad44645bd
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-06-25 21:12:18 +0000

    Update generated code for v1099

--- billing_metereventsummary.go
+++ billing_metereventsummary.go
@@ -18,7 +18,7 @@ type BillingMeterEventSummaryListParams struct {
 	Expand []*string `form:"expand"`
 	// The timestamp from when to start aggregating meter events (inclusive).
 	StartTime *int64 `form:"start_time"`
-	// Specifies what granularity to use when generating event summaries. If not specified, a single event summary would be returned for the specified time range.
+	// Specifies what granularity to use when generating event summaries. If not specified, a single event summary would be returned for the specified time range. For hourly granularity, start and end times must align with hour boundaries (e.g., 00:00, 01:00, ..., 23:00). For daily granularity, start and end times must align with UTC day boundaries (00:00 UTC).
 	ValueGroupingWindow *string `form:"value_grouping_window"`
 }
 
