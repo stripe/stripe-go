23ac9a4c25ad10a160875e32c5ec39d1bf5f6e6c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-27 17:15:47 +0000

    Update generated code for v1220

---
+++ terminal/readercollecteddata/client.go
@@ -0,0 +1,37 @@
+//
+//
+// File generated from our OpenAPI spec
+//
+//
+
+// Package readercollecteddata provides the /terminal/reader_collected_data APIs
+package readercollecteddata
+
+import (
+	"net/http"
+
+	stripe "github.com/stripe/stripe-go/v79"
+)
+
+// Client is used to invoke /terminal/reader_collected_data APIs.
+type Client struct {
+	B   stripe.Backend
+	Key string
+}
+
+// Retrieve data collected using Reader hardware.
+func Get(id string, params *stripe.TerminalReaderCollectedDataParams) (*stripe.TerminalReaderCollectedData, error) {
+	return getC().Get(id, params)
+}
+
+// Retrieve data collected using Reader hardware.
+func (c Client) Get(id string, params *stripe.TerminalReaderCollectedDataParams) (*stripe.TerminalReaderCollectedData, error) {
+	path := stripe.FormatURLPath("/v1/terminal/reader_collected_data/%s", id)
+	readercollecteddata := &stripe.TerminalReaderCollectedData{}
+	err := c.B.Call(http.MethodGet, path, c.Key, params, readercollecteddata)
+	return readercollecteddata, err
+}
+
+func getC() Client {
+	return Client{stripe.GetBackend(stripe.APIBackend), stripe.Key}
+}
