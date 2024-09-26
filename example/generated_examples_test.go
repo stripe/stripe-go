fbda9fac3fab9d947333244198f9334ab4b53edb
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-09-26 16:22:04 +0000

    Update generated code for v1268

--- example/generated_examples_test.go
+++ example/generated_examples_test.go
@@ -3088,19 +3088,6 @@ func TestTerminalReadersProcessPaymentIntentPost(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-func TestTerminalReadersProcessSetupIntentPost(t *testing.T) {
-	params := &stripe.TerminalReaderProcessSetupIntentParams{
-		SetupIntent:    stripe.String("seti_xxxxxxxxxxxxx"),
-		AllowRedisplay: stripe.String("always"),
-	}
-	result, err := terminal_reader.ProcessSetupIntent(
-		"tmr_xxxxxxxxxxxxx",
-		params,
-	)
-	assert.NotNil(t, result)
-	assert.Nil(t, err)
-}
-
 func TestTestHelpersCustomersFundCashBalancePost(t *testing.T) {
 	params := &stripe.TestHelpersCustomerFundCashBalanceParams{
 		Amount:   stripe.Int64(30),
