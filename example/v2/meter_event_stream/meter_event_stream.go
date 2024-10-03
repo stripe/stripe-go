61114773bfa1d1e12670e1d43872dca3738c51b3
Jesse Rosalia <jar@stripe.com>
2024-10-03 14:44:47 -0700

    added rawrequest.Client type and associated functions, to let us specify a backend and key with which to make raw requests added GetRawRequestBackend helper function to stripe, to get back a RawRequestBackend or error added MeterEventsBackend and MeterEventsURL to constants, and added MeterEventsBackend to GetBackendWithConfig switch statement
    rewrote meter_event_stream.go example to use rawrequest.Client

--- example/v2/meter_event_stream/meter_event_stream.go
+++ example/v2/meter_event_stream/meter_event_stream.go
@@ -10,36 +10,15 @@ import (
 	rawrequest "github.com/stripe/stripe-go/v80/rawrequest"
 )
 
-// TODO: add rawrequest.Client, add MeterEvents backend, clean this up
-
-type MeterEventSessionResponse struct {
-	Object              string // "v2.billing.meter_event_session"
-	AuthenticationToken string `json:"authentication_token"`
-	ExpiresAt           string `json:"expires_at"`
-}
-
-type MeterEventStreamEventPayload struct {
-	StripeCustomerID string `json:"stripe_customer_id"`
-	Value            string `json:"value"`
-}
-
-type MeterEventStreamEventParams struct {
-	EventName string                       `json:"event_name"`
-	Payload   MeterEventStreamEventPayload `json:"payload"`
-}
-type MeterEventStreamParams struct {
-	Events []MeterEventStreamEventParams `json:"events"`
-}
-
 var sessionAuthToken string = ""
 var sessionAuthExpiresAt string = ""
 
-func refreshMeterEventSession() (err error) {
+func refreshMeterEventSession(client rawrequest.Client) (err error) {
 	currentTime := time.Now().Format(time.RFC3339)
 	// Check if session is null or expired
 	if sessionAuthToken == "" || sessionAuthExpiresAt <= currentTime {
 		// Create a new meter event session in case the existing session expired
-		rawResp, err := rawrequest.Post("/v2/billing/meter_event_session", "", nil)
+		rawResp, err := client.Post("/v2/billing/meter_event_session", "", nil)
 		if err != nil {
 			return err
 		}
@@ -47,72 +26,36 @@ func refreshMeterEventSession() (err error) {
 			return fmt.Errorf(rawResp.Status)
 		}
 
-		/* Example using map[string]interface{}
 		var resp map[string]interface{}
 		err = json.Unmarshal(rawResp.RawJSON, &resp)
 		if err != nil {
 			return err
 		}
-		// sessionAuthToken = resp["authentication_token"].(string)
-		// sessionAuthExpiresAt = resp["expires_at"].(string)
-		*/
 
-		// Example using structs
-		var resp MeterEventSessionResponse
-		// var resp map[string]interface{}
-		err = json.Unmarshal(rawResp.RawJSON, &resp)
-		if err != nil {
-			return err
-		}
-		sessionAuthToken = resp.AuthenticationToken
-		sessionAuthExpiresAt = resp.ExpiresAt
+		sessionAuthToken = resp["authentication_token"].(string)
+		sessionAuthExpiresAt = resp["expires_at"].(string)
+
 		fmt.Println("Meter event session created!")
 	}
 	return nil
 }
 
-var oldKey = ""
-var meterEventsURL = "https://meter-events.stripe.com"
-
-func prepareMeterEventsCall(authToken string) {
-	fmt.Println("Preparing for meter events call")
-	backend := stripe.GetBackendWithConfig(
-		stripe.APIBackend,
-		&stripe.BackendConfig{
-			HTTPClient:        nil,
-			LeveledLogger:     nil, // Set by GetBackendWithConfiguation when nil
-			MaxNetworkRetries: nil, // Set by GetBackendWithConfiguation when nil
-			URL:               &meterEventsURL,
-		},
-	)
-
-	stripe.SetBackend(stripe.APIBackend, backend)
-	oldKey = stripe.Key
-	stripe.Key = authToken
-}
-
-func cleanupMeterEventsCall() {
-	fmt.Println("Cleaning up after meter events call")
-	stripe.SetBackend(stripe.APIBackend, nil)
-	stripe.Key = oldKey
-	oldKey = ""
-}
-
-func sendMeterEvent(eventName string, stripeCustomerID string, value string) (err error) {
+func sendMeterEvent(client rawrequest.Client, eventName string, stripeCustomerID string, value string) (err error) {
 	// Refresh the meter event session if necessary
-	refreshMeterEventSession()
+	refreshMeterEventSession(client)
 
 	if sessionAuthToken == "" {
 		err = fmt.Errorf("Unable to refresh meter event session")
 		return
 	}
 
-	// Change the APIBackend to point to meter-events.stripe.com
-	// to use rawrequest.Post to call a meter event stream API
-	prepareMeterEventsCall(sessionAuthToken)
-	defer cleanupMeterEventsCall()
+	b, err := stripe.GetRawRequestBackend(stripe.MeterEventsBackend)
+	if err != nil {
+		return err
+	}
 
-	/* Example using map[string]interface{}
+	sessionClient := rawrequest.Client{B: b, Key: sessionAuthToken}
+
 	params := map[string]interface{}{
 		"events": []interface{}{
 			map[string]interface{}{
@@ -124,35 +67,31 @@ func sendMeterEvent(eventName string, stripeCustomerID string, value string) (er
 			},
 		},
 	}
-	*/
-	// Example using structs
-	params := MeterEventStreamParams{
-		Events: []MeterEventStreamEventParams{
-			{
-				EventName: eventName,
-				Payload: MeterEventStreamEventPayload{
-					StripeCustomerID: stripeCustomerID,
-					Value:            value,
-				},
-			},
-		},
-	}
 
 	contentBytes, err := json.Marshal(params)
 	if err != nil {
 		return
 	}
+
 	content := string(contentBytes)
-	_, err = rawrequest.Post("/v2/billing/meter_event_stream", content, nil)
+	_, err = sessionClient.Post("/v2/billing/meter_event_stream", content, nil)
 	return
 }
 
 func main() {
 
-	stripe.Key = "{{API_KEY}}"
+	apiKey := "{{API_KEY}}"
 	customerID := "{{CUSTOMER_ID}}"
 
-	err := sendMeterEvent("api_requests", customerID, "25")
+	b, err := stripe.GetRawRequestBackend(stripe.APIBackend)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	client := rawrequest.Client{B: b, Key: apiKey}
+
+	err = sendMeterEvent(client, "api_requests", customerID, "25")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- rawrequest/client.go
+++ rawrequest/client.go
@@ -7,12 +7,32 @@ import (
 	stripe "github.com/stripe/stripe-go/v80"
 )
 
+// Client is used to invoke /quotes APIs.
+type Client struct {
+	B   stripe.RawRequestBackend
+	Key string
+}
+
 func Get(path string, params *stripe.RawParams) (*stripe.APIResponse, error) {
 	return stripe.RawRequest(http.MethodGet, path, "", params)
 }
+
+func (c Client) Get(path string, params *stripe.RawParams) (*stripe.APIResponse, error) {
+	return c.B.RawRequest(http.MethodGet, path, c.Key, "", params)
+}
+
 func Post(path, content string, params *stripe.RawParams) (*stripe.APIResponse, error) {
 	return stripe.RawRequest(http.MethodPost, path, content, params)
 }
+
+func (c Client) Post(path, content string, params *stripe.RawParams) (*stripe.APIResponse, error) {
+	return c.B.RawRequest(http.MethodPost, path, c.Key, content, params)
+}
+
 func Delete(path string, params *stripe.RawParams) (*stripe.APIResponse, error) {
 	return stripe.RawRequest(http.MethodDelete, path, "", params)
+}
+
+func (c Client) Delete(path string, params *stripe.RawParams) (*stripe.APIResponse, error) {
+	return c.B.RawRequest(http.MethodDelete, path, c.Key, "", params)
 }
