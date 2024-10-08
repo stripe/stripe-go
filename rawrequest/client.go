c4e75242f21402d43ca45af37f23eaa018c9cae2
jar-stripe <jar@stripe.com>
2024-10-07 17:55:09 -0700

    raw_request.Client + V2 UBB Examples (#1929)
    * added rawrequest.Client type and associated functions, to let us specify a backend and key with which to make raw requests
    
    * added GetRawRequestBackend helper function to stripe, to get back a RawRequestBackend or error
    
    * added MeterEventsBackend and MeterEventsURL to constants, and added MeterEventsBackend to GetBackendWithConfig switch statement
    
    * added UBB examples

--- rawrequest/client.go
+++ rawrequest/client.go
@@ -7,12 +7,24 @@ import (
 	stripe "github.com/stripe/stripe-go/v80"
 )
 
+// Client is used to invoke raw requests against the specified backend
+type Client struct {
+	B   stripe.RawRequestBackend
+	Key string
+}
+
+func (c Client) RawRequest(method string, path string, content string, params *stripe.RawParams) (*stripe.APIResponse, error) {
+	return c.B.RawRequest(method, path, c.Key, content, params)
+}
+
 func Get(path string, params *stripe.RawParams) (*stripe.APIResponse, error) {
 	return stripe.RawRequest(http.MethodGet, path, "", params)
 }
+
 func Post(path, content string, params *stripe.RawParams) (*stripe.APIResponse, error) {
 	return stripe.RawRequest(http.MethodPost, path, content, params)
 }
+
 func Delete(path string, params *stripe.RawParams) (*stripe.APIResponse, error) {
 	return stripe.RawRequest(http.MethodDelete, path, "", params)
 }
