a846c7f76d431ed03565a6ea85ad043df1027517
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-11-11 23:46:18 +0000

    Update generated code for v1340

--- issuing_card.go
+++ issuing_card.go
@@ -383,7 +383,7 @@ type IssuingCard struct {
 	CancellationReason IssuingCardCancellationReason `json:"cancellation_reason"`
 	// An Issuing `Cardholder` object represents an individual or business entity who is [issued](https://stripe.com/docs/issuing) cards.
 	//
-	// Related guide: [How to create a cardholder](https://stripe.com/docs/issuing/cards#create-cardholder)
+	// Related guide: [How to create a cardholder](https://stripe.com/docs/issuing/cards/virtual/issue-cards#create-cardholder)
 	Cardholder *IssuingCardholder `json:"cardholder"`
 	// Time at which the object was created. Measured in seconds since the Unix epoch.
 	Created int64 `json:"created"`
