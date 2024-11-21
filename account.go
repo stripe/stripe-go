03cc01d53946a3860fd5f52bd422bbb54fac7d3a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-11-21 21:08:42 +0000

    Update generated code for v1371

--- account.go
+++ account.go
@@ -267,7 +267,7 @@ type AccountParams struct {
 	//
 	// By default, providing an external account sets it as the new default external account for its currency, and deletes the old default if one exists. To add additional external accounts without replacing the existing default for the currency, use the [bank account](https://stripe.com/api#account_create_bank_account) or [card creation](https://stripe.com/api#account_create_card) APIs. After you create an [Account Link](https://stripe.com/api/account_links) or [Account Session](https://stripe.com/api/account_sessions), this property can only be updated for accounts where [controller.requirement_collection](https://stripe.com/api/accounts/object#account_object-controller-requirement_collection) is `application`, which includes Custom accounts.
 	ExternalAccount *AccountExternalAccountParams `form:"external_account"`
-	// A hash of account group type to tokens. These are account groups this account should be added to
+	// A hash of account group type to tokens. These are account groups this account should be added to.
 	Groups *AccountGroupsParams `form:"groups"`
 	// Information about the person represented by the account. This field is null unless `business_type` is set to `individual`. Once you create an [Account Link](https://stripe.com/api/account_links) or [Account Session](https://stripe.com/api/account_sessions), this property can only be updated for accounts where [controller.requirement_collection](https://stripe.com/api/accounts/object#account_object-controller-requirement_collection) is `application`, which includes Custom accounts.
 	Individual *PersonParams `form:"individual"`
@@ -1070,7 +1070,7 @@ func (p *AccountExternalAccountParams) AddMetadata(key string, value string) {
 	p.Metadata[key] = value
 }
 
-// A hash of account group type to tokens. These are account groups this account should be added to
+// A hash of account group type to tokens. These are account groups this account should be added to.
 type AccountGroupsParams struct {
 	// The group the account is in to determine their payments pricing, and null if the account is on customized pricing. [See the Platform pricing tool documentation](https://stripe.com/docs/connect/platform-pricing-tools) for details.
 	PaymentsPricing *string `form:"payments_pricing"`
