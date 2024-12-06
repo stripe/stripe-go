599235532802c571c06ab5cb93546ca1b597b458
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-12-06 16:29:06 +0000

    Update generated code for v1387

--- issuing_authorization.go
+++ issuing_authorization.go
@@ -111,6 +111,7 @@ const (
 	IssuingAuthorizationRequestHistoryReasonCardholderVerificationRequired IssuingAuthorizationRequestHistoryReason = "cardholder_verification_required"
 	IssuingAuthorizationRequestHistoryReasonInsecureAuthorizationMethod    IssuingAuthorizationRequestHistoryReason = "insecure_authorization_method"
 	IssuingAuthorizationRequestHistoryReasonInsufficientFunds              IssuingAuthorizationRequestHistoryReason = "insufficient_funds"
+	IssuingAuthorizationRequestHistoryReasonNetworkFallback                IssuingAuthorizationRequestHistoryReason = "network_fallback"
 	IssuingAuthorizationRequestHistoryReasonNotAllowed                     IssuingAuthorizationRequestHistoryReason = "not_allowed"
 	IssuingAuthorizationRequestHistoryReasonPINBlocked                     IssuingAuthorizationRequestHistoryReason = "pin_blocked"
 	IssuingAuthorizationRequestHistoryReasonSpendingControls               IssuingAuthorizationRequestHistoryReason = "spending_controls"
