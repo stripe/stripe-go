49d55d2dd8983c9cd28723adfa98b2613835da3a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-01-14 16:10:01 +0000

    Update generated code for v1444

--- checkout_session.go
+++ checkout_session.go
@@ -3661,7 +3661,7 @@ type CheckoutSessionSavedPaymentMethodOptions struct {
 // When set, provides configuration for Checkout to collect a shipping address from a customer.
 type CheckoutSessionShippingAddressCollection struct {
 	// An array of two-letter ISO country codes representing which countries Checkout should provide as options for
-	// shipping locations. Unsupported country codes: `AS, CX, CC, CU, HM, IR, KP, MH, FM, NF, MP, PW, SD, SY, UM, VI`.
+	// shipping locations. Unsupported country codes: `AS, CX, CC, CU, HM, IR, KP, MH, FM, NF, MP, PW, SY, UM, VI`.
 	AllowedCountries []string `json:"allowed_countries"`
 }
 
