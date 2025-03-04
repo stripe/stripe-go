27f20467300e0ed0a3b42b44eb4423dd25b6a5d1
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-03-04 18:35:43 +0000

    Update generated code for v1524

--- subscription.go
+++ subscription.go
@@ -1205,7 +1205,7 @@ type Subscription struct {
 	NextPendingInvoiceItemInvoice int64 `json:"next_pending_invoice_item_invoice"`
 	// String representing the object's type. Objects of the same type share the same value.
 	Object string `json:"object"`
-	// The account (if any) the charge was made on behalf of for charges associated with this subscription. See the Connect documentation for details.
+	// The account (if any) the charge was made on behalf of for charges associated with this subscription. See the [Connect documentation](https://stripe.com/docs/connect/subscriptions#on-behalf-of) for details.
 	OnBehalfOf *Account `json:"on_behalf_of"`
 	// If specified, payment collection for this subscription will be paused. Note that the subscription status will be unchanged and will not be updated to `paused`. Learn more about [pausing collection](https://stripe.com/docs/billing/subscriptions/pause-payment).
 	PauseCollection *SubscriptionPauseCollection `json:"pause_collection"`
