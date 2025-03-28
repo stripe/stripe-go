be6bc463514d03b0d1c7c9f16771e8d2073bc4e6
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-03-28 19:13:31 +0000

    Update generated code for v1638

--- event.go
+++ event.go
@@ -257,6 +257,13 @@ const (
 	EventTypeTreasuryReceivedCreditFailed                       EventType = "treasury.received_credit.failed"
 	EventTypeTreasuryReceivedCreditSucceeded                    EventType = "treasury.received_credit.succeeded"
 	EventTypeTreasuryReceivedDebitCreated                       EventType = "treasury.received_debit.created"
+	EventTypeBillingCreditBalanceTransactionCreated             EventType = "billing.credit_balance_transaction.created"
+	EventTypeBillingCreditGrantCreated                          EventType = "billing.credit_grant.created"
+	EventTypeBillingCreditGrantUpdated                          EventType = "billing.credit_grant.updated"
+	EventTypeBillingMeterCreated                                EventType = "billing.meter.created"
+	EventTypeBillingMeterDeactivated                            EventType = "billing.meter.deactivated"
+	EventTypeBillingMeterReactivated                            EventType = "billing.meter.reactivated"
+	EventTypeBillingMeterUpdated                                EventType = "billing.meter.updated"
 )
 
 // List events, going back up to 30 days. Each event data is rendered according to Stripe API version at its creation time, specified in [event object](https://docs.stripe.com/api/events/object) api_version attribute (not according to your current Stripe API version or Stripe-Version header).
