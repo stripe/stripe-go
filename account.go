3195b9c141e8101285ec56b800176ce078acc7c8
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-07-09 16:54:35 +0000

    Update generated code for v1826 and 2025-06-30.preview

--- account.go
+++ account.go
@@ -289,7 +289,7 @@ const (
 //
 // Test-mode accounts can be deleted at any time.
 //
-// Live-mode accounts where Stripe is responsible for negative account balances cannot be deleted, which includes Standard accounts. Live-mode accounts where your platform is liable for negative account balances, which includes Custom and Express accounts, can be deleted when all [balances](https://docs.stripe.com/api/balance/balance_object) are zero.
+// Live-mode accounts that have access to the standard dashboard and Stripe is responsible for negative account balances cannot be deleted, which includes Standard accounts. All other Live-mode accounts, can be deleted when all [balances](https://docs.stripe.com/api/balance/balance_object) are zero.
 //
 // If you want to delete your own account, use the [account information tab in your account settings](https://dashboard.stripe.com/settings/account) instead.
 type AccountParams struct {
@@ -1534,7 +1534,7 @@ func (p *AccountRejectParams) AddExpand(f string) {
 //
 // Test-mode accounts can be deleted at any time.
 //
-// Live-mode accounts where Stripe is responsible for negative account balances cannot be deleted, which includes Standard accounts. Live-mode accounts where your platform is liable for negative account balances, which includes Custom and Express accounts, can be deleted when all [balances](https://docs.stripe.com/api/balance/balance_object) are zero.
+// Live-mode accounts that have access to the standard dashboard and Stripe is responsible for negative account balances cannot be deleted, which includes Standard accounts. All other Live-mode accounts, can be deleted when all [balances](https://docs.stripe.com/api/balance/balance_object) are zero.
 //
 // If you want to delete your own account, use the [account information tab in your account settings](https://dashboard.stripe.com/settings/account) instead.
 type AccountDeleteParams struct {
