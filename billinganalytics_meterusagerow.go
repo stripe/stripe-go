33aea0982ea12b43a51f7fa9e1540f0196990558
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-09-17 22:58:45 +0000

    Update generated code for v1980 and 2025-09-30.preview

--- billinganalytics_meterusagerow.go
+++ billinganalytics_meterusagerow.go
@@ -7,18 +7,25 @@
 package stripe
 
 type BillingAnalyticsMeterUsageRow struct {
-	// Timestamp indicating the end of the bucket. Measured in seconds since the Unix epoch.
-	BucketEndTime int64 `json:"bucket_end_time"`
-	// Timestamp indicating the start of the bucket. Measured in seconds since the Unix epoch.
-	BucketStartTime int64 `json:"bucket_start_time"`
-	// The aggregated meter usage value for the specified bucket.
-	BucketValue float64 `json:"bucket_value"`
 	// A set of key-value pairs representing the dimensions of the meter usage.
 	Dimensions map[string]string `json:"dimensions"`
+	// Timestamp indicating the end of the bucket. Measured in seconds since the Unix epoch.
+	EndsAt int64 `json:"ends_at"`
 	// Unique identifier for the object.
 	ID string `json:"id"`
 	// The unique identifier for the meter. Null if no meters were provided and usage was aggregated across all meters.
-	MeterID string `json:"meter_id"`
+	Meter string `json:"meter"`
 	// String representing the object's type. Objects of the same type share the same value.
 	Object string `json:"object"`
+	// Timestamp indicating the start of the bucket. Measured in seconds since the Unix epoch.
+	StartsAt int64 `json:"starts_at"`
+	// The aggregated meter usage value for the specified bucket.
+	Value float64 `json:"value"`
+}
+
+// BillingAnalyticsMeterUsageRowList is a list of MeterUsageRows as retrieved from a list endpoint.
+type BillingAnalyticsMeterUsageRowList struct {
+	APIResource
+	ListMeta
+	Data []*BillingAnalyticsMeterUsageRow `json:"data"`
 }
