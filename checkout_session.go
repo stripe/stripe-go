bbc9df566527cf7163ae5a9fcc78b2b644480599
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-09-22 23:21:26 +0000

    Update generated code for v2017 and 2025-09-30.preview

--- checkout_session.go
+++ checkout_session.go
@@ -46,6 +46,24 @@ const (
 	CheckoutSessionBrandingSettingsBorderStyleRounded     CheckoutSessionBrandingSettingsBorderStyle = "rounded"
 )
 
+// The type of image for the icon. Must be one of `file` or `url`.
+type CheckoutSessionBrandingSettingsIconType string
+
+// List of values that CheckoutSessionBrandingSettingsIconType can take
+const (
+	CheckoutSessionBrandingSettingsIconTypeFile CheckoutSessionBrandingSettingsIconType = "file"
+	CheckoutSessionBrandingSettingsIconTypeURL  CheckoutSessionBrandingSettingsIconType = "url"
+)
+
+// The type of image for the logo. Must be one of `file` or `url`.
+type CheckoutSessionBrandingSettingsLogoType string
+
+// List of values that CheckoutSessionBrandingSettingsLogoType can take
+const (
+	CheckoutSessionBrandingSettingsLogoTypeFile CheckoutSessionBrandingSettingsLogoType = "file"
+	CheckoutSessionBrandingSettingsLogoTypeURL  CheckoutSessionBrandingSettingsLogoType = "url"
+)
+
 // The type of the tax ID, one of `ad_nrt`, `ar_cuit`, `eu_vat`, `bo_tin`, `br_cnpj`, `br_cpf`, `cn_tin`, `co_nit`, `cr_tin`, `do_rcn`, `ec_ruc`, `eu_oss_vat`, `hr_oib`, `pe_ruc`, `ro_tin`, `rs_pib`, `sv_nit`, `uy_ruc`, `ve_rif`, `vn_tin`, `gb_vat`, `nz_gst`, `au_abn`, `au_arn`, `in_gst`, `no_vat`, `no_voec`, `za_vat`, `ch_vat`, `mx_rfc`, `sg_uen`, `ru_inn`, `ru_kpp`, `ca_bn`, `hk_br`, `es_cif`, `tw_vat`, `th_vat`, `jp_cn`, `jp_rn`, `jp_trn`, `li_uid`, `li_vat`, `my_itn`, `us_ein`, `kr_brn`, `ca_qst`, `ca_gst_hst`, `ca_pst_bc`, `ca_pst_mb`, `ca_pst_sk`, `my_sst`, `sg_gst`, `ae_trn`, `cl_tin`, `sa_vat`, `id_npwp`, `my_frp`, `il_vat`, `ge_vat`, `ua_vat`, `is_vat`, `bg_uic`, `hu_tin`, `si_tin`, `ke_pin`, `tr_tin`, `eg_tin`, `ph_tin`, `al_tin`, `bh_vat`, `kz_bin`, `ng_tin`, `om_vat`, `de_stn`, `ch_uid`, `tz_vat`, `uz_vat`, `uz_tin`, `md_vat`, `ma_vat`, `by_tin`, `ao_tin`, `bs_tin`, `bb_tin`, `cd_nif`, `mr_nif`, `me_pib`, `zw_tin`, `ba_tin`, `gn_nif`, `mk_vat`, `sr_fin`, `sn_ninea`, `am_tin`, `np_pan`, `tj_tin`, `ug_tin`, `zm_tin`, `kh_tin`, `aw_tin`, `az_tin`, `bd_bin`, `bj_ifu`, `et_tin`, `kg_tin`, `la_tin`, `cm_niu`, `cv_nif`, `bf_ifu`, or `unknown`
 type CheckoutSessionCollectedInformationTaxIDType string
 
@@ -5566,10 +5584,24 @@ type CheckoutSessionAutomaticTax struct {
 }
 
 // The icon for the Checkout Session. You cannot set both `logo` and `icon`.
-type CheckoutSessionBrandingSettingsIcon struct{}
+type CheckoutSessionBrandingSettingsIcon struct {
+	// The ID of a [File upload](https://stripe.com/docs/api/files) representing the icon. Purpose must be `business_icon`. Required if `type` is `file` and disallowed otherwise.
+	File string `json:"file"`
+	// The type of image for the icon. Must be one of `file` or `url`.
+	Type CheckoutSessionBrandingSettingsIconType `json:"type"`
+	// The URL of the image. Present when `type` is `url`.
+	URL string `json:"url"`
+}
 
 // The logo for the Checkout Session. You cannot set both `logo` and `icon`.
-type CheckoutSessionBrandingSettingsLogo struct{}
+type CheckoutSessionBrandingSettingsLogo struct {
+	// The ID of a [File upload](https://stripe.com/docs/api/files) representing the logo. Purpose must be `business_logo`. Required if `type` is `file` and disallowed otherwise.
+	File string `json:"file"`
+	// The type of image for the logo. Must be one of `file` or `url`.
+	Type CheckoutSessionBrandingSettingsLogoType `json:"type"`
+	// The URL of the image. Present when `type` is `url`.
+	URL string `json:"url"`
+}
 type CheckoutSessionBrandingSettings struct {
 	// A hex color value starting with `#` representing the background color for the Checkout Session.
 	BackgroundColor string `json:"background_color"`
