0bf3f8174d5cc6f12d9a97579fcc0c371626ecb2
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-09-23 18:23:48 +0000

    Update generated code for v2020 and 2025-09-30.preview

--- promotioncode.go
+++ promotioncode.go
@@ -44,9 +44,9 @@ func (p *PromotionCodeListParams) AddExpand(f string) {
 
 // The promotion referenced by this promotion code.
 type PromotionCodePromotionParams struct {
-	// If the promotion type is 'coupon', the coupon for this promotion code.
+	// If promotion `type` is `coupon`, the coupon for this promotion code.
 	Coupon *string `form:"coupon"`
-	// Specifies the type of promotion code.
+	// Specifies the type of promotion.
 	Type *string `form:"type"`
 }
 
@@ -111,9 +111,9 @@ func (p *PromotionCodeParams) AddMetadata(key string, value string) {
 
 // The promotion referenced by this promotion code.
 type PromotionCodeCreatePromotionParams struct {
-	// If the promotion type is 'coupon', the coupon for this promotion code.
+	// If promotion `type` is `coupon`, the coupon for this promotion code.
 	Coupon *string `form:"coupon"`
-	// Specifies the type of promotion code.
+	// Specifies the type of promotion.
 	Type *string `form:"type"`
 }
 
@@ -228,7 +228,7 @@ func (p *PromotionCodeUpdateParams) AddMetadata(key string, value string) {
 }
 
 type PromotionCodePromotion struct {
-	// If promotion type is 'coupon', the coupon for this promotion.
+	// If promotion `type` is `coupon`, the coupon for this promotion.
 	Coupon *Coupon `json:"coupon"`
 	// The type of promotion.
 	Type PromotionCodePromotionType `json:"type"`
