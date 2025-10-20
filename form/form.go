20d6ea2298491a8106b6a1cb09ec431c3f21758e
Prathmesh Ranaut <prathmesh@stripe.com>
2025-10-20 12:29:05 -0400

    Added flat_array structtag to V2 APIs to fix query param encoding

--- form/form.go
+++ form/form.go
@@ -71,6 +71,13 @@ type formOptions struct {
 	// adequate solution in the vast majority of cases and has a usability
 	// benefit, so we've gone this route.
 	HighPrecision bool
+
+	// Gets rid of the index and returns unindexed array when using
+	// form encoding in v2 APIs
+	// Eg. include=["foo", "bar] is encoded when:
+	// FlatArray = false as include[0]=foo&include[1]=bar
+	// FlatArray = true  as include=foo&include=bar
+	FlatArray bool
 }
 
 type structEncoder struct {
@@ -187,7 +194,10 @@ func buildArrayOrSliceEncoder(t reflect.Type) encoderFunc {
 		var arrNames []string
 
 		for i := 0; i < v.Len(); i++ {
-			arrNames = append(keyParts, strconv.Itoa(i))
+			arrNames = keyParts
+			if options == nil || !options.FlatArray {
+				arrNames = append(keyParts, strconv.Itoa(i))
+			}
 
 			indexV := v.Index(i)
 			elemF(values, indexV, arrNames, indexV.Kind() == reflect.Ptr, nil)
@@ -408,6 +418,7 @@ func makeStructEncoder(t reflect.Type) *structEncoder {
 	// `form`
 	se := &structEncoder{}
 
+	// validation step
 	for i := 0; i < t.NumField(); i++ {
 		reflectField := t.Field(i)
 		tag := reflectField.Tag.Get(tagName)
@@ -532,6 +543,12 @@ func parseTag(tag string) (string, *formOptions) {
 				options = &formOptions{}
 			}
 			options.HighPrecision = true
+
+		case "flat_array":
+			if options == nil {
+				options = &formOptions{}
+			}
+			options.FlatArray = true
 
 		default:
 			if Strict {
