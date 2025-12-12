977943bbb0c261b9413da425d523b9c624d5513c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-12-12 20:41:39 +0000

    Update generated code for v2140 and

--- invoicelineitem.go
+++ invoicelineitem.go
@@ -532,6 +532,8 @@ type InvoiceLineItem struct {
 	// The quantity of the subscription, if the line item is a subscription or a proration.
 	Quantity     int64         `json:"quantity"`
 	Subscription *Subscription `json:"subscription"`
+	// The subtotal of the line item, in cents (or local equivalent), before any discounts or taxes.
+	Subtotal int64 `json:"subtotal"`
 	// The tax information of the line item.
 	Taxes []*InvoiceLineItemTax `json:"taxes"`
 }
