de00677c207a46d9f9cafe7774c3d3bdcb967b17
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-02-18 23:04:42 +0000

    Update generated code for v2178 and

--- billing_alerttriggered.go
+++ billing_alerttriggered.go
@@ -6,7 +6,35 @@
 
 package stripe
 
+// The type of grouping used for this alert notification
+type BillingAlertTriggeredGroupByType string
+
+// List of values that BillingAlertTriggeredGroupByType can take
+const (
+	BillingAlertTriggeredGroupByTypeBillingCadence          BillingAlertTriggeredGroupByType = "billing_cadence"
+	BillingAlertTriggeredGroupByTypePricingPlanSubscription BillingAlertTriggeredGroupByType = "pricing_plan_subscription"
+)
+
+// The aggregation period for which this alert triggered
+type BillingAlertTriggeredAggregationPeriod struct {
+	// End time of the aggregation period
+	EndsAt int64 `json:"ends_at"`
+	// Start time of the aggregation period
+	StartsAt int64 `json:"starts_at"`
+}
+
+// Populated specifically for spend alerts to notify merchants which group_by entity has the exceeded spend
+type BillingAlertTriggeredGroupBy struct {
+	// The billing cadence ID, populated when type is `billing_cadence`
+	BillingCadence string `json:"billing_cadence"`
+	// The pricing plan subscription ID, populated when type is `pricing_plan_subscription`
+	PricingPlanSubscription string `json:"pricing_plan_subscription"`
+	// The type of grouping used for this alert notification
+	Type BillingAlertTriggeredGroupByType `json:"type"`
+}
 type BillingAlertTriggered struct {
+	// The aggregation period for which this alert triggered
+	AggregationPeriod *BillingAlertTriggeredAggregationPeriod `json:"aggregation_period"`
 	// A billing alert is a resource that notifies you when a certain usage threshold on a meter is crossed. For example, you might create a billing alert to notify you when a certain user made 100 API requests.
 	Alert *BillingAlert `json:"alert"`
 	// Time at which the object was created. Measured in seconds since the Unix epoch.
@@ -19,10 +47,14 @@ type BillingAlertTriggered struct {
 	CustomPricingUnit string `json:"custom_pricing_unit"`
 	// External customer ID for the customer for which the alert triggered
 	ExternalCustomerID string `json:"external_customer_id"`
+	// Populated specifically for spend alerts to notify merchants which group_by entity has the exceeded spend
+	GroupBy *BillingAlertTriggeredGroupBy `json:"group_by"`
 	// Has the value `true` if the object exists in live mode or the value `false` if the object exists in test mode.
 	Livemode bool `json:"livemode"`
 	// String representing the object's type. Objects of the same type share the same value.
 	Object string `json:"object"`
+	// Timestamp at which the threshold was crossed
+	TriggeredAt int64 `json:"triggered_at"`
 	// The value triggering the alert
 	Value float64 `json:"value,string"`
 }
