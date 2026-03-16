442cbd8b32d8ad92d30f681dfa5f0349a02c2535
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-03-16 22:31:55 +0000

    Update generated code for v2202 and

--- v2core_account_params.go
+++ v2core_account_params.go
@@ -763,10 +763,18 @@ type V2CoreAccountIdentityBusinessDetailsAddressParams struct {
 	Town *string `form:"town" json:"town,omitempty"`
 }
 
+// A non-negative integer representing the amount in the smallest currency unit.
+type V2CoreAccountIdentityBusinessDetailsAnnualRevenueAmountParams struct {
+	// Three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html), in lowercase. Must be a [supported currency](https://stripe.com/docs/currencies).
+	Currency *string `form:"currency" json:"currency,omitempty"`
+	// A non-negative integer representing how much to charge in the [smallest currency unit](https://docs.stripe.com/currencies#minor-units).
+	Value *int64 `form:"value" json:"value,omitempty"`
+}
+
 // The business gross annual revenue for its preceding fiscal year.
 type V2CoreAccountIdentityBusinessDetailsAnnualRevenueParams struct {
 	// A non-negative integer representing the amount in the smallest currency unit.
-	Amount *Amount `form:"amount" json:"amount,omitempty"`
+	Amount *V2CoreAccountIdentityBusinessDetailsAnnualRevenueAmountParams `form:"amount" json:"amount,omitempty"`
 	// The close-out date of the preceding fiscal year in ISO 8601 format. E.g. 2023-12-31 for the 31st of December, 2023.
 	FiscalYearEnd *string `form:"fiscal_year_end" json:"fiscal_year_end,omitempty"`
 }
@@ -893,10 +901,18 @@ type V2CoreAccountIdentityBusinessDetailsIDNumberParams struct {
 	Value *string `form:"value" json:"value"`
 }
 
+// A non-negative integer representing the amount in the smallest currency unit.
+type V2CoreAccountIdentityBusinessDetailsMonthlyEstimatedRevenueAmountParams struct {
+	// Three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html), in lowercase. Must be a [supported currency](https://stripe.com/docs/currencies).
+	Currency *string `form:"currency" json:"currency,omitempty"`
+	// A non-negative integer representing how much to charge in the [smallest currency unit](https://docs.stripe.com/currencies#minor-units).
+	Value *int64 `form:"value" json:"value,omitempty"`
+}
+
 // An estimate of the monthly revenue of the business.
 type V2CoreAccountIdentityBusinessDetailsMonthlyEstimatedRevenueParams struct {
 	// A non-negative integer representing the amount in the smallest currency unit.
-	Amount *Amount `form:"amount" json:"amount,omitempty"`
+	Amount *V2CoreAccountIdentityBusinessDetailsMonthlyEstimatedRevenueAmountParams `form:"amount" json:"amount,omitempty"`
 }
 
 // When the business was incorporated or registered.
@@ -2073,10 +2089,18 @@ type V2CoreAccountCreateIdentityBusinessDetailsAddressParams struct {
 	Town *string `form:"town" json:"town,omitempty"`
 }
 
+// A non-negative integer representing the amount in the smallest currency unit.
+type V2CoreAccountCreateIdentityBusinessDetailsAnnualRevenueAmountParams struct {
+	// Three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html), in lowercase. Must be a [supported currency](https://stripe.com/docs/currencies).
+	Currency *string `form:"currency" json:"currency,omitempty"`
+	// A non-negative integer representing how much to charge in the [smallest currency unit](https://docs.stripe.com/currencies#minor-units).
+	Value *int64 `form:"value" json:"value,omitempty"`
+}
+
 // The business gross annual revenue for its preceding fiscal year.
 type V2CoreAccountCreateIdentityBusinessDetailsAnnualRevenueParams struct {
 	// A non-negative integer representing the amount in the smallest currency unit.
-	Amount *Amount `form:"amount" json:"amount,omitempty"`
+	Amount *V2CoreAccountCreateIdentityBusinessDetailsAnnualRevenueAmountParams `form:"amount" json:"amount,omitempty"`
 	// The close-out date of the preceding fiscal year in ISO 8601 format. E.g. 2023-12-31 for the 31st of December, 2023.
 	FiscalYearEnd *string `form:"fiscal_year_end" json:"fiscal_year_end,omitempty"`
 }
@@ -2203,10 +2227,18 @@ type V2CoreAccountCreateIdentityBusinessDetailsIDNumberParams struct {
 	Value *string `form:"value" json:"value"`
 }
 
+// A non-negative integer representing the amount in the smallest currency unit.
+type V2CoreAccountCreateIdentityBusinessDetailsMonthlyEstimatedRevenueAmountParams struct {
+	// Three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html), in lowercase. Must be a [supported currency](https://stripe.com/docs/currencies).
+	Currency *string `form:"currency" json:"currency,omitempty"`
+	// A non-negative integer representing how much to charge in the [smallest currency unit](https://docs.stripe.com/currencies#minor-units).
+	Value *int64 `form:"value" json:"value,omitempty"`
+}
+
 // An estimate of the monthly revenue of the business.
 type V2CoreAccountCreateIdentityBusinessDetailsMonthlyEstimatedRevenueParams struct {
 	// A non-negative integer representing the amount in the smallest currency unit.
-	Amount *Amount `form:"amount" json:"amount,omitempty"`
+	Amount *V2CoreAccountCreateIdentityBusinessDetailsMonthlyEstimatedRevenueAmountParams `form:"amount" json:"amount,omitempty"`
 }
 
 // When the business was incorporated or registered.
@@ -3397,10 +3429,18 @@ type V2CoreAccountUpdateIdentityBusinessDetailsAddressParams struct {
 	Town *string `form:"town" json:"town,omitempty"`
 }
 
+// A non-negative integer representing the amount in the smallest currency unit.
+type V2CoreAccountUpdateIdentityBusinessDetailsAnnualRevenueAmountParams struct {
+	// Three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html), in lowercase. Must be a [supported currency](https://stripe.com/docs/currencies).
+	Currency *string `form:"currency" json:"currency,omitempty"`
+	// A non-negative integer representing how much to charge in the [smallest currency unit](https://docs.stripe.com/currencies#minor-units).
+	Value *int64 `form:"value" json:"value,omitempty"`
+}
+
 // The business gross annual revenue for its preceding fiscal year.
 type V2CoreAccountUpdateIdentityBusinessDetailsAnnualRevenueParams struct {
 	// A non-negative integer representing the amount in the smallest currency unit.
-	Amount *Amount `form:"amount" json:"amount,omitempty"`
+	Amount *V2CoreAccountUpdateIdentityBusinessDetailsAnnualRevenueAmountParams `form:"amount" json:"amount,omitempty"`
 	// The close-out date of the preceding fiscal year in ISO 8601 format. E.g. 2023-12-31 for the 31st of December, 2023.
 	FiscalYearEnd *string `form:"fiscal_year_end" json:"fiscal_year_end,omitempty"`
 }
@@ -3527,10 +3567,18 @@ type V2CoreAccountUpdateIdentityBusinessDetailsIDNumberParams struct {
 	Value *string `form:"value" json:"value"`
 }
 
+// A non-negative integer representing the amount in the smallest currency unit.
+type V2CoreAccountUpdateIdentityBusinessDetailsMonthlyEstimatedRevenueAmountParams struct {
+	// Three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html), in lowercase. Must be a [supported currency](https://stripe.com/docs/currencies).
+	Currency *string `form:"currency" json:"currency,omitempty"`
+	// A non-negative integer representing how much to charge in the [smallest currency unit](https://docs.stripe.com/currencies#minor-units).
+	Value *int64 `form:"value" json:"value,omitempty"`
+}
+
 // An estimate of the monthly revenue of the business.
 type V2CoreAccountUpdateIdentityBusinessDetailsMonthlyEstimatedRevenueParams struct {
 	// A non-negative integer representing the amount in the smallest currency unit.
-	Amount *Amount `form:"amount" json:"amount,omitempty"`
+	Amount *V2CoreAccountUpdateIdentityBusinessDetailsMonthlyEstimatedRevenueAmountParams `form:"amount" json:"amount,omitempty"`
 }
 
 // When the business was incorporated or registered.
