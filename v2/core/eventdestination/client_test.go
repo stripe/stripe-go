9669646e075cf3379ea6166c0ce72086edd7ad43
Prathmesh Ranaut <165320323+prathmesh-stripe@users.noreply.github.com>
2026-03-17 16:39:41 -0400

    Updated go event destination tests (#2303)

--- v2/core/eventdestination/client_test.go
+++ v2/core/eventdestination/client_test.go
@@ -39,7 +39,7 @@ func TestEventDestinationNew(t *testing.T) {
 			Description:   *params.Description,
 			EnabledEvents: enabledEvents,
 			EventPayload:  stripe.V2CoreEventDestinationEventPayload(*params.EventPayload),
-			EventsFrom:    []stripe.V2CoreEventDestinationEventsFrom{stripe.V2CoreEventDestinationEventsFromSelf},
+			EventsFrom:    []string{"@self"},
 			Name:          *params.Name,
 			Status:        stripe.V2CoreEventDestinationStatusEnabled,
 			Type:          stripe.V2CoreEventDestinationType(*params.Type),
@@ -66,7 +66,7 @@ func TestEventDestinationNew(t *testing.T) {
 	assert.Equal(t, dest.Status, stripe.V2CoreEventDestinationStatusEnabled)
 	assert.True(t, dest.Created.Equal(timeNow))
 	assert.True(t, dest.Updated.Equal(timeNow))
-	assert.Equal(t, dest.EventsFrom, []stripe.V2CoreEventDestinationEventsFrom{stripe.V2CoreEventDestinationEventsFromSelf})
+	assert.Equal(t, dest.EventsFrom, []string{"@self"})
 }
 
 func TestEventDestinationGet(t *testing.T) {
@@ -80,7 +80,7 @@ func TestEventDestinationGet(t *testing.T) {
 			Description:   "This is my event destination, I like it a lot",
 			EnabledEvents: []string{"v1.billing.meter.error_report_triggered"},
 			EventPayload:  stripe.V2CoreEventDestinationEventPayloadThin,
-			EventsFrom:    []stripe.V2CoreEventDestinationEventsFrom{stripe.V2CoreEventDestinationEventsFromSelf},
+			EventsFrom:    []string{"@self"},
 			Name:          "My Event Destination",
 			Status:        stripe.V2CoreEventDestinationStatusEnabled,
 			Type:          stripe.V2CoreEventDestinationTypeWebhookEndpoint,
@@ -104,7 +104,7 @@ func TestEventDestinationGet(t *testing.T) {
 	assert.Equal(t, dest.Status, stripe.V2CoreEventDestinationStatusEnabled)
 	assert.True(t, dest.Created.Equal(timeNow))
 	assert.True(t, dest.Updated.Equal(timeNow))
-	assert.Equal(t, dest.EventsFrom, []stripe.V2CoreEventDestinationEventsFrom{stripe.V2CoreEventDestinationEventsFromSelf})
+	assert.Equal(t, dest.EventsFrom, []string{"@self"})
 	assert.Equal(t, dest.ID, "ed_test_61RM8ltWcTW4mbsxf16RJyfa2xSQLHJJh1sxm7H0KVT6")
 }
 
@@ -119,7 +119,7 @@ func TestEventDestinationUpdate(t *testing.T) {
 			Description:   "Better description",
 			EnabledEvents: []string{"v1.billing.meter.error_report_triggered"},
 			EventPayload:  stripe.V2CoreEventDestinationEventPayloadThin,
-			EventsFrom:    []stripe.V2CoreEventDestinationEventsFrom{stripe.V2CoreEventDestinationEventsFromSelf},
+			EventsFrom:    []string{"@self"},
 			Name:          "My Event Destination",
 			Status:        stripe.V2CoreEventDestinationStatusEnabled,
 			Type:          stripe.V2CoreEventDestinationTypeWebhookEndpoint,
@@ -141,7 +141,7 @@ func TestEventDestinationUpdate(t *testing.T) {
 	assert.Equal(t, dest.Status, stripe.V2CoreEventDestinationStatusEnabled)
 	assert.True(t, dest.Created.Equal(timeNow))
 	assert.True(t, dest.Updated.Equal(timeNow))
-	assert.Equal(t, dest.EventsFrom, []stripe.V2CoreEventDestinationEventsFrom{stripe.V2CoreEventDestinationEventsFromSelf})
+	assert.Equal(t, dest.EventsFrom, []string{"@self"})
 	assert.Equal(t, dest.ID, "ed_test_61RM8ltWcTW4mbsxf16RJyfa2xSQLHJJh1sxm7H0KVT6")
 }
 
@@ -167,7 +167,7 @@ func TestEventDestinationDisable(t *testing.T) {
 			Description:   "This is my event destination, I like it a lot",
 			EnabledEvents: []string{"v1.billing.meter.error_report_triggered"},
 			EventPayload:  stripe.V2CoreEventDestinationEventPayloadThin,
-			EventsFrom:    []stripe.V2CoreEventDestinationEventsFrom{stripe.V2CoreEventDestinationEventsFromSelf},
+			EventsFrom:    []string{"@self"},
 			Name:          "My Event Destination",
 			Status:        stripe.V2CoreEventDestinationStatusDisabled,
 			Type:          stripe.V2CoreEventDestinationTypeWebhookEndpoint,
@@ -192,7 +192,7 @@ func TestEventDestinationEnable(t *testing.T) {
 			Description:   "This is my event destination, I like it a lot",
 			EnabledEvents: []string{"v1.billing.meter.error_report_triggered"},
 			EventPayload:  stripe.V2CoreEventDestinationEventPayloadThin,
-			EventsFrom:    []stripe.V2CoreEventDestinationEventsFrom{stripe.V2CoreEventDestinationEventsFromSelf},
+			EventsFrom:    []string{"@self"},
 			Name:          "My Event Destination",
 			Status:        stripe.V2CoreEventDestinationStatusEnabled,
 			Type:          stripe.V2CoreEventDestinationTypeWebhookEndpoint,
