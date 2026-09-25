a7e57d968334243ef720ce2dda6dd7c5fe6cd790
Jesse Rosalia <jar@stripe.com>
2026-09-24 18:13:22 -0700

    Revert "Represent free-form objects as dynamic maps (#2440)"
    This reverts commit 2a37dbd0343455c9ad09819685ef5f7321beb337.

--- coupon_test.go
+++ coupon_test.go
@@ -18,13 +18,12 @@ func TestCoupon_UnmarshalJSON(t *testing.T) {
 
 	// Unmarshals from a JSON object
 	{
-		v := Coupon{ID: "25OFF", Script: &CouponScript{Configuration: map[string]any{"nested": map[string]any{"key": "value"}}}}
+		v := Coupon{ID: "25OFF"}
 		data, err := json.Marshal(&v)
 		assert.NoError(t, err)
 
 		err = json.Unmarshal(data, &v)
 		assert.NoError(t, err)
 		assert.Equal(t, "25OFF", v.ID)
-		assert.Equal(t, "value", v.Script.Configuration["nested"].(map[string]interface{})["key"])
 	}
 }
